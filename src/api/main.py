1f6b8109aff38808857ba659af68fd66eb93d670
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2015-05-04 14:52:05 +0200

    [webui] Add CORS support in api for development

--- src/api/main.py
+++ src/api/main.py
@@ -1,3 +1,4 @@
+import flask
 from flask.ext.restful import Api
 
 from api import app, apiv1, session
@@ -6,6 +7,24 @@ from api import app, apiv1, session
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     session.remove()
+
+"""
+DEVELOPMENT ONLY!
+"""
+# TODO: Figure out how to deal with this in production.
+@app.after_request
+def add_cors(resp):
+    """ Ensure all responses have the CORS headers. This ensures any failures are also accessible
+        by the client. """
+    resp.headers['Access-Control-Allow-Origin'] = flask.request.headers.get('Origin','*')
+    resp.headers['Access-Control-Allow-Credentials'] = 'true'
+    resp.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS, GET'
+    resp.headers['Access-Control-Allow-Headers'] = flask.request.headers.get(
+        'Access-Control-Request-Headers', 'Authorization' )
+    # set low for debugging
+    if app.debug:
+        resp.headers['Access-Control-Max-Age'] = '1'
+    return resp
 
 api = Api(app)
 
