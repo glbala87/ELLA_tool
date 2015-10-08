288e1c9d9eca2008e4cb888a1941fed499b3ab01
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2015-10-08 12:43:13 +0200

    [deposit] Fix broken reference importing

--- src/vardb/deposit/deposit_references.py
+++ src/vardb/deposit/deposit_references.py
@@ -7,7 +7,11 @@ REFERENCE_PATH = os.path.join(SCRIPT_DIR, '../testdata/references.csv')
 
 
 def import_references():
-    print db.engine.execute('COPY reference FROM \'{}\' WITH CSV HEADER'.format(REFERENCE_PATH))
+    conn = db.engine.connect()
+    trans = conn.begin()
+    result = conn.execute('COPY reference FROM \'{}\' WITH CSV HEADER'.format(REFERENCE_PATH))
+    trans.commit()
+    conn.close()
 
 
 if __name__ == '__main__':
