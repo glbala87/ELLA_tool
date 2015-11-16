28fde28fc6f09524bb186320239d516f9e507cf3
vbhavdal <vbhavdal@gmail.com>
2015-11-16 09:08:21 +0100

    Use a custom field type to serialize the returned dict

--- src/api/schemas/classifications.py
+++ src/api/schemas/classifications.py
@@ -2,26 +2,51 @@ from marshmallow import fields, Schema
 from gre.grm import GRM 
 from gre.grc import ClassificationResult
 
+# From https://github.com/marshmallow-code/marshmallow/issues/120#issuecomment-81382070 , the built in Dict doesn't work. 
+class DictField(fields.Field):
+
+    def __init__(self, key_field, nested_field, *args, **kwargs):
+        fields.Field.__init__(self, *args, **kwargs)
+        self.key_field = key_field
+        self.nested_field = nested_field
+
+    def _deserialize(self, value):
+        ret = {}
+        for key, val in value.items():
+            k = self.key_field.deserialize(key)
+            v = self.nested_field.deserialize(val)
+            ret[k] = v
+        return ret
+
+    def _serialize(self, value, attr, obj):
+        ret = {}
+        for key, val in value.items():
+            k = self.key_field._serialize(key, attr, obj)
+            v = self.nested_field.serialize(key, self.get_value(attr, obj))
+            ret[k] = v
+        return ret
+
 class Rule(fields.Field):
     def _serialize(self, value, attr, o):
-        if isinstance(o, ClassificationResult) :
-            return {"class" : o.clazz, "classification" : o.classification, "message" : o.message, "contributors" : o.contributors}
-        elif isinstance(o, GRM.AndRule):
-            return {"class" : o.__class__.__name__, "$and" : [r for r in o.subrules]}
-        elif isinstance(o, GRM.OrRule):
-            return {"class" : o.__class__.__name__, "$or" : [r for r in o.subrules]}
-        elif isinstance(o, GRM.NotRule):
-            return {"class" : o.__class__.__name__, "$not" : o.subrule}
-        elif isinstance(o, GRM.AtLeastRule):
-            return {"class" : o.__class__.__name__, "atleast": o.atleast, "value" : o.value, "code" : o.code}
-        elif isinstance(o, GRM.Rule):
-            return {"class" : o.__class__.__name__, "value" : o.value, "source" : o.source, "code" : o.code}
+        if isinstance(value, list):
+            return [self._serialize(e, attr, o) for e in value]
+        if isinstance(value, ClassificationResult) :
+            return {"class" : value.clazz, "classification" : value.classification, "message" : value.message, "contributors" : value.contributors}
+        elif isinstance(value, GRM.AndRule):
+            return {"class" : value.__class__.__name__, "$and" : self._serialize(value.subrules, attr, o) }
+        elif isinstance(value, GRM.OrRule):
+            return {"class" : value.__class__.__name__, "$or" : self._serialize(value.subrules, attr, o) }
+        elif isinstance(value, GRM.NotRule):
+            return {"class" : value.__class__.__name__, "$not" : self._serialize(value.subrule, attr,o)}
+        elif isinstance(value, GRM.AtLeastRule):
+            return {"class" : value.__class__.__name__, "atleast": value.atleast, "value" : value.value, "code" : value.code}
+        elif isinstance(value, GRM.Rule):
+            return {"class" : value.__class__.__name__, "value" : value.value, "source" : value.source, "code" : value.code}
+        else:
+            raise RuntimeError("Can't serialize: " + value)
 
 class ClassificationSchema(Schema):
     class Meta:
-        fields = ("class",
-                  "classification",
-                  "contributors",
-                  "message")
-        contributors = Rule(many = True)
+        fields = ["alleles"]
+    alleles = DictField(fields.Int(), Rule())
     
