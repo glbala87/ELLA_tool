098acac21ba738e202c3a5699c5f93974a745106
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2015-12-02 14:02:39 +0100

    [api] Move config to backend and serve to frontend

--- src/webui/src/js/services/ConfigService.js
+++ src/webui/src/js/services/ConfigService.js
@@ -1,61 +1,41 @@
 /* jshint esnext: true */
 
 
-import {Service} from '../ng-decorators';
+import {Service, Inject} from '../ng-decorators';
 
 @Service({
     serviceName: 'Config'
 })
+@Inject('$resource')
 class ConfigService {
+
+    constructor(resource) {
+        this.resource = resource;
+        this.config = null;
+    }
+
+    /**
+     * "Hack" that probably will bite me later:
+     * Expect config to be loaded on startup,
+     * then return cached object. Otherwise we have
+     * to introduce Promises into everywhere in the app
+     * where config is used...
+     * Right now it's loaded in Analysis directive.
+     */
+    loadConfig() {
+        return new Promise((resolve, reject) => {
+            let r = this.resource('/api/v1/config/');
+            let config = r.get(() => {
+                this.config = config;
+                resolve(config);
+            });
+        });
+    }
+
     getConfig() {
-        return {
-            frequencies: {
-                'ExAC': [
-                    'G',
-                    'AFR',
-                    'AMR',
-                    'EAS',
-                    'FIN',
-                    'NFE',
-                    'OTH',
-                    'SAS',
-                ],
-                '1000g': [
-                    'G',
-                    'AMR',
-                    'ASN',
-                    'EUR',
-                    'EAS',
-                    'SAS'
-                ],
-                'esp6500': [
-                    'AA',
-                    'EA',
-                ],
-                'inDB': [
-                    'alleleFreq',
-                ]
-            },
-            freq_criteria: {
-                'ExAC': {
-                    'G': 0.01
-                },
-                '1000g': {
-                    'G': 0.01
-                },
-                'inDB': {
-                    'alleleFreq': 0.05
-                }
-            },
-            variant_criteria: {
-                intronic_region: {
-                    '-': 20,
-                    '+': 6
-                }
-            },
-            alleleassessment: {
-                days_since_update: 300
-            }
-        };
+        if (!this.config) {
+            throw Error("Config not loaded, call loadConfig() first.");
+        }
+        return this.config;
     }
 }
