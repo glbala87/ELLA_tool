ee60207764da0e110cd248468e1dea96ebddcdc6
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-01-13 10:21:33 +0100

    [api] Implement request_json decorator

--- src/api/util/util.py
+++ src/api/util/util.py
@@ -1,6 +1,6 @@
 import json
 from flask import request
-from api import db
+from api import db, ApiError
 
 
 def error(msg, code):
@@ -53,3 +53,50 @@ def paginate(func):
         kwargs['num_per_page'] = num_per_page
         return func(*args, **kwargs)
     return inner
+
+
+def request_json(required, only_required=False, allowed=None):
+    """
+    Decorator: Checks flasks's request json object for 'required'
+    fields before passing on the data to the function.
+
+    If 'only_required', the json input is "washed" so only
+    the fields in required are passed on.
+
+    If 'allowed' is set, the json input is "washed" so only
+    those fields are passed on.
+    """
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            data = request.get_json()
+            for field in required:
+                if not data.get(field):
+                    raise ApiError("Missing or empty required field {} in provided data.".format(field))
+
+            if only_required:
+                data = {k: v for k, v in data.iteritems() if k in required}
+            elif allowed:
+                data = {k: v for k, v in data.iteritems() if k in allowed}
+            return func(*args, data=data, **kwargs)
+        return inner
+    return wrapper
+
+
+def deepmerge(source, destination):
+    """
+    http://stackoverflow.com/a/20666342
+
+    >>> a = { 'first' : { 'all_rows' : { 'pass' : 'dog', 'number' : '1' } } }
+    >>> b = { 'first' : { 'all_rows' : { 'fail' : 'cat', 'number' : '5' } } }
+    >>> merge(b, a) == { 'first' : { 'all_rows' : { 'pass' : 'dog', 'fail' : 'cat', 'number' : '5' } } }
+    True
+    """
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            deepmerge(value, node)
+        else:
+            destination[key] = value
+
+    return destination
