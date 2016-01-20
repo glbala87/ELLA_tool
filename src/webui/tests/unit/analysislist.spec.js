812627b4bb6c03d641a03dad45de5ebf058f0a2e
Erik Severinsen <erik.severinsen@medisin.uio.no>
2016-01-20 14:28:37 +0100

    cleanup of analysis diretive test

--- src/webui/tests/unit/analysislist.spec.js
+++ src/webui/tests/unit/analysislist.spec.js
@@ -2,7 +2,6 @@
 
     var scope;
     var elm;
-    var compiled_elm;
 
     beforeEach(function() {
         angular.mock.module('workbench');
@@ -13,22 +12,20 @@
         elm = angular.element("<analysis-list analyses='vm.pending_analyses'></analysis-list>");
         scope = $rootScope.$new();
         scope.vm = {
-            //pending_analyses: [{'name': 'one'}, {'name': 'two'}] // two items ng-repeat in the template
+            //pending_analyses: [
+            //    {'name': 'one', getInterpretationState: function() {return undefined;}},
+            //    {'name': 'two', getInterpretationState: function() {return undefined;}}
+            //] // two items ng-repeat in the template
             pending_analyses: [undefined] // a single item to kick off ng-repeat in the template
         };
 
-        compiled_elm = $compile(elm)(scope);
+        $compile(elm)(scope);
         scope.$digest();
-
     }));
-
 
     it("list is expanded", function () {
       expect(elm.find('div')).toBeDefined();
       expect(elm.find('div').length).toBeGreaterThan(1);
-
     });
-
-
 
 });
