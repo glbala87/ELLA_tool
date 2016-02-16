01f36b6a5d1f39382c9c633da334beb96e4098a7
Erik Severinsen <erik.severinsen@medisin.uio.no>
2016-02-16 13:27:10 +0100

    fixed test: setup of config

--- src/webui/src/js/index.js
+++ src/webui/src/js/index.js
@@ -85,7 +85,7 @@ class AppConfig {
                         };
                     }],
                     controllerAs: 'vm'
-                },
+                }
             }
         })
         .state('app.analyses', {
