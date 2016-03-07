11b9a11dd0ba04e97bd18af1d14ddc9b9c48d00c
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-03-07 16:15:49 +0100

    [webui] Further work on report. Almost there now.

--- src/webui/src/js/widgets/reportcard/reportcard.directive.js
+++ src/webui/src/js/widgets/reportcard/reportcard.directive.js
@@ -31,6 +31,8 @@ export class ReportCardController {
         this.alleleService = Allele;
         this.interpretationService = Interpretation;
         this.analysisService = Analysis;
+
+        this.selected_excluded = null;
     }
 
     getIncludedAlleles() {
@@ -53,14 +55,45 @@ export class ReportCardController {
         });
     }
 
+    includeSelectedExcluded() {
+        if (this.selected_excluded) {
+            if (!('report' in this.state.allele[this.selected_excluded.id])) {
+                this.state.allele[this.selected_excluded.id].report = {};
+            };
+            this.state.allele[this.selected_excluded.id].report.included = true;
+        }
+        this.selected_excluded = null;
+    }
 
     getAlleleState(allele) {
         return this.state.allele[allele.id];
     }
 
-    getAlleleAssessment(allele) {
-        this.getAlleleState(allele).alleleassessment;
+    getClassification(allele) {
+        if ('alleleassessment' in this.getAlleleState(allele) &&
+            'classification' in this.getAlleleState(allele).alleleassessment) {
+            return this.getAlleleState(allele).alleleassessment.classification;
+        }
     }
 
+    formatHGVS(allele) {
+        let hgvs = '';
+        for (let t of allele.annotation.filtered) {
+            hgvs += `${t.Transcript}.${t.Transcript_version}(${t.SYMBOL}):`;
+            let part = t.HGVSc_short.split("c.", 2)[1]; // remove 'c.'
+            if (allele.genotype.homozygous) {
+                hgvs += `c.[${part}];[(${part})]`; // c.[76A>C];[(76A>C)]
+            }
+            else {
+                hgvs += `c.[${part}];[=]`; // c.[76A>C];[=]
+            }
+            let classification = this.getClassification(allele);
+            if (classification) {
+                hgvs += ` ${this.config.report.classification_text[classification]}`;
+            }
+        }
+        hgvs += ``;
+        return hgvs;
+    }
 
 }
