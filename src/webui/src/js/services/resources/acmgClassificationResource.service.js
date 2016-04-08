129fa81945cb3dc8f9c3cbcfc6106cf17fd5685c
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-04-08 15:29:01 +0200

    [webui] Rework webui to support changes in ACMG resource

--- src/webui/src/js/services/resources/acmgClassificationResource.service.js
+++ src/webui/src/js/services/resources/acmgClassificationResource.service.js
@@ -14,19 +14,18 @@ class ACMGClassificationResource {
         this.base = '/api/v1';
     }
 
-   getByAlleleIds(allele_ids, gp_name, gp_version, refassessment_ids=null) {
-        let uri = `${this.base}/acmg/alleles/?allele_ids=${allele_ids}&gp_name=${gp_name}&gp_version=${gp_version}`;
-        if (refassessment_ids) {
-            uri += `&reference_assessment_ids=${refassessment_ids}`;
+   getByAlleleIds(allele_ids, gp_name, gp_version, referenceassessments=null) {
+        let uri = `${this.base}/acmg/alleles/`;
+
+        let data = {
+            allele_ids: allele_ids,
+            gp_name: gp_name,
+            gp_version: gp_version,
+            referenceassessments: referenceassessments
         }
         return new Promise((resolve, reject) => {
-            let r = this.resource(uri);
-            let classification = r.get(
-                () => {
-                    resolve(classification);
-
-                }
-            );
+            let r = this.resource(uri, null, {getCodes: {method: 'POST'}});
+            let classification = r.getCodes(data, resolve, reject);
         });
    }
 }
