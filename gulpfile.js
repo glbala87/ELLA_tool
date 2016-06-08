86d89caba1481e167d52dc0a673c824037dca747
Erik Severinsen <erik.severinsen@medisin.uio.no>
2016-06-08 15:00:30 +0200

    renamed spec file

--- gulpfile.js
+++ gulpfile.js
@@ -168,7 +168,7 @@ gulp.task('e2e', function(done) {
     // spec orders matter until we can handle the alert popup when swithcing pages
     gulp.src([
              './src/webui/tests/e2e/misc.spec.js',
-             './src/webui/tests/e2e/allele-popup-spec.js'
+             './src/webui/tests/e2e/allele-popup.spec.js'
         ])
         .pipe(protractor({
             configFile: "./src/webui/tests/protractor.conf.js",
