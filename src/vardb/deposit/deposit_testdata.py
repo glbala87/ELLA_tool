3dd286804548e31d846de42e24ce404de6fe42a0
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-06-13 16:31:22 +0200

    [deposit] Fix deposit_test script after requiring sample/analysis configs

--- src/vardb/deposit/deposit_testdata.py
+++ src/vardb/deposit/deposit_testdata.py
@@ -18,6 +18,8 @@ from vardb.deposit.deposit_references import import_references
 from vardb.deposit.deposit_users import import_users
 from vardb.deposit.deposit import Importer
 
+from vardb.util import vcfiterator
+
 log = logging.getLogger(__name__)
 
 SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -90,6 +92,10 @@ class DepositTestdata(object):
         self.engine = db.engine
         self.session = db.session
 
+    def _get_vcf_samples(self, vcf_path):
+        vi = vcfiterator.VcfIterator(vcf_path)
+        return vi.getSamples()
+
     def remake_db(self):
         # We must import all models before recreating database
         from vardb.datamodel import allele, genotype, assessment, sample, patient, disease, gene, annotation  # needed
@@ -109,7 +115,6 @@ class DepositTestdata(object):
         if test_set is None:
             testset = next(v for v in VCF if v.get('default'))
         else:
-            print test_set
             testset = next(v for v in VCF if v['name'] == test_set)
 
         vcf_paths = glob.glob(os.path.join(SCRIPT_DIR, testset['path'], '*.vcf'))
@@ -117,16 +122,31 @@ class DepositTestdata(object):
         for vcf_path in vcf_paths:
             importer = Importer(self.session)
             try:
+                vcf_path = os.path.join(SCRIPT_DIR, vcf_path)
                 filename = os.path.basename(vcf_path)
                 # Get last part of filename before ext 'sample.HBOC_v00.vcf'
                 gp_part = os.path.splitext(filename)[0].split('.')[-1].split('_')
+
+                sample_name = os.path.splitext(filename)[0].split('.')[-1].split('_')
                 kwargs = {
                     'genepanel_name': gp_part[0],
                     'genepanel_version': gp_part[1],
-                    'import_assessments': testset.get('import_assessments', False)
+                    'import_assessments': testset.get('import_assessments', False),
+                    'sample_configs': [{
+                        'name': self._get_vcf_samples(vcf_path)[0]
+                    }],
+                    'analysis_config': {
+                        'name': '{}-{}-{}'.format(
+                            self._get_vcf_samples(vcf_path)[0],
+                            *gp_part
+                        ),
+                        'params': {
+                            'genepanel': '_'.join(gp_part)
+                        }
+                    }
                 }
                 importer.importVcf(
-                    os.path.join(SCRIPT_DIR, vcf_path),
+                    vcf_path,
                     **kwargs
                 )
                 log.info("Deposited {}".format(vcf_path))
