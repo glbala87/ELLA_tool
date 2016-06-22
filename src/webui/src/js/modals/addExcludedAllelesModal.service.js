209ca5f3cd00c0b57d432ef11e22c99ef828b769
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-06-22 18:17:24 +0200

    [webui] Fix addExcludedAllelesModal after backend filtering changes

--- src/webui/src/js/modals/addExcludedAllelesModal.service.js
+++ src/webui/src/js/modals/addExcludedAllelesModal.service.js
@@ -7,27 +7,61 @@ export class AddExcludedAllelesController {
      */
 
     constructor(modalInstance,
-                $timeout,
                 Config,
-                AlleleFilter,
-                excluded_alleles,
-                included_allele_ids) {
+                Allele,
+                excluded_allele_ids,
+                included_allele_ids,
+                sample_id,
+                gp_name,
+                gp_version) {
         this.modal = modalInstance;
-        this.timeout = $timeout;
         this.frequencies = Config.getConfig().frequencies;
-        this.alleleFilter = AlleleFilter;
-        this.master_alleles = excluded_alleles;
-        this.category_excluded_alleles = []; // Alleles filtered on the category, but no further filtering
-        this.excluded_alleles = [];  // Finished filtered Alleles to show, that are not yet added
-        this.included_alleles = [];  // Alleles added by user (internal use)
+        this.alleleService = Allele;
         this.included_allele_ids = included_allele_ids;  // Alleles added by user
+        this.alleles = {
+            class1: [],
+            intronic: []
+        }; // Alleles loaded from backend
+        this.excluded_allele_ids = excluded_allele_ids;
+        this.category_excluded_alleles = []; // Alleles filtered on the category, but no further filtering. Needed for showing genes
+        this.filtered_alleles = [];  // Finished filtered Alleles to show, that are not yet added
+        this.included_alleles = [];  // Alleles added by user (internal use)
         this.category = 'all';
         this.slice = []; // Holds the slice of alleles for pagination
         this.gene_options = []; // Options for gene selection dropdown
         this.selected_gene = null;
         this.page_idx = 1;
-        this.number_per_page = 5;
-        this.update();
+        this.number_per_page = 3;
+
+        this.loadAlleles(sample_id, gp_name, gp_version);
+    }
+
+    /**
+     * Load in all allele data from backend (yes, all x hundreds at once, we're lazy...)
+     * TODO: Improve performance with pagination if necessary
+     */
+    loadAlleles(sample_id, gp_name, gp_version) {
+
+        // Class 1
+        let p_class1 = this.alleleService.getAlleles(
+            this.excluded_allele_ids.class1,
+            sample_id,
+            gp_name,
+            gp_version
+        ).then(alleles => this.alleles.class1 = alleles);
+
+        // Intronic
+        let p_intronic = this.alleleService.getAlleles(
+            this.excluded_allele_ids.intronic,
+            sample_id,
+            gp_name,
+            gp_version
+        ).then(alleles => this.alleles.intronic = alleles);
+
+        // When done, update view data
+        Promise.all([p_class1, p_intronic]).then(
+            () => this.update()
+        )
     }
 
     update() {
@@ -48,57 +82,49 @@ export class AddExcludedAllelesController {
     }
 
     fromId(id) {
-        return this.master_alleles.find(a => a.id === id);
+        return this.alleles.find(a => a.id === id);
     }
 
     _updateFilter() {
         if (this.category === 'all') {
-            this.category_excluded_alleles = this.master_alleles.slice(0); // Copy array
+            this.category_excluded_alleles = this.alleles.class1.concat(this.alleles.intronic);
         }
         else if (this.category === 'class_one') {
-            this.category_excluded_alleles = this.alleleFilter.invert(
-                this.alleleFilter.filterClass1(this.master_alleles),
-                this.master_alleles
-            );
+            this.category_excluded_alleles = this.alleles.class1;
         }
         else if (this.category === 'intronic') {
-            // Only show intronic variants that are not class 1
-            // hence start with non-class-1
-            let non_class1 = this.alleleFilter.filterClass1(this.master_alleles);
-
-            this.category_excluded_alleles = this.alleleFilter.invert(
-                this.alleleFilter.filterIntronicAlleles(non_class1),
-                non_class1
-            );
+            this.category_excluded_alleles = this.alleles.intronic;
         }
 
-        // Clone array to excluded_alleles, to update the display list
-        this.excluded_alleles = this.category_excluded_alleles.slice(0);
+        this.filtered_alleles = this.category_excluded_alleles.slice(0); // Clone array
 
         // Filter out included alleles
         if (this.included_allele_ids.length) {
-            this.excluded_alleles = this.excluded_alleles.filter(a => {
+            this.filtered_alleles = this.filtered_alleles.filter(a => {
                 return this.included_allele_ids.find(ia => ia === a.id) === undefined;
             });
         }
 
         // Filter by gene symbol
         if (this.selected_gene) {
-            this.excluded_alleles = this.excluded_alleles.filter(a => {
-                return a.annotation.filtered[0].SYMBOL === this.selected_gene;
+            this.filtered_alleles = this.filtered_alleles.filter(a => {
+                return a.annotation.filtered.some(t => {
+                    if ('SYMBOL' in t) {
+                        return t.SYMBOL === this.selected_gene;
+                    };
+                })
             });
         }
 
         // Sort by gene symbol and HGVSc
-        this.excluded_alleles.sort(
+        this.filtered_alleles.sort(
             firstBy(v => v.annotation.filtered[0].SYMBOL)
             .thenBy(v => v.annotation.filtered[0].HGVSc)
         );
-
     }
 
     _updateSlice() {
-        this.slice = this.excluded_alleles.slice(
+        this.slice = this.filtered_alleles.slice(
             (this.page_idx-1) * this.number_per_page,
             this.page_idx * this.number_per_page
         );
@@ -118,8 +144,9 @@ export class AddExcludedAllelesController {
     }
 
     _updateIncludedAlleles() {
+        let all_alleles = this.alleles.class1.concat(this.alleles.intronic);
         this.included_alleles = this.included_allele_ids.map(
-            aid => this.master_alleles.find(ma => ma.id === aid)
+            aid => all_alleles.find(ma => ma.id === aid)
         );
     }
 }
@@ -140,20 +167,36 @@ export class AddExcludedAllelesModal {
 
     /**
      * Popups a dialog for adding excluded alleles
-     * @param  {Array} excluded_alleles List of alleles that are excluded
+     * @param  {Object} excluded_allele_ids Object with list of alleles that are excluded {class1: [..], intronic: [..]}
      * @param  {Array} included_allele_ids List of allele ids that are included
-     *                 (must be subset of excluded_alleles). This is directly updated based on users selection.
+     *                 (must be subset of excluded_allele_ids). This is directly updated based on users selection.
+     * @param  {int} sample_id Sample for which to load alleles
+     * @param  {string} gp_name Genepanel name for which to load alleles
+     * @param  {string} gp_version Genepanel version for which to load alleles
      * @return {Promise} Promise that resolves when dialog is closed. Resolves with same array as included_allele_ids.
      */
-    show(excluded_alleles, included_allele_ids) {
+    show(excluded_allele_ids, included_allele_ids, sample_id, gp_name, gp_version) {
 
         let modal = this.modalService.open({
             templateUrl: 'ngtmpl/addExcludedAllelesModal.ngtmpl.html',
-            controller: ['$uibModalInstance', '$timeout', 'Config', 'AlleleFilter', 'excluded_alleles', 'included_allele_ids', AddExcludedAllelesController],
+            controller: [
+                '$uibModalInstance',
+                'Config',
+                'Allele',
+                'excluded_allele_ids',
+                'included_allele_ids',
+                'sample_id',
+                'gp_name',
+                'gp_version',
+                AddExcludedAllelesController
+            ],
             controllerAs: 'vm',
             resolve: {
-                excluded_alleles: () => excluded_alleles,
+                excluded_allele_ids: () => excluded_allele_ids,
                 included_allele_ids: () => included_allele_ids,
+                sample_id: () => sample_id,
+                gp_name: () => gp_name,
+                gp_version: () => gp_version
             },
             size: 'lg'
         });
