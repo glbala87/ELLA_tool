d29b71814146a24d4fc7da08c0c98811f44178e5
Erik Severinsen <erik.severinsen@medisin.uio.no>
2016-08-09 14:29:56 +0200

    moved file; syntax checking finds schema file by relative path

--- src/vardb/deposit/deposit_genepanel.py
+++ src/vardb/deposit/deposit_genepanel.py
@@ -63,9 +63,10 @@ def load_transcripts(transcripts_path):
 
 
 def config_valid(config):
-    filename = 'src/vardb/datamodel/genap-genepanel-config-schema.json'
+    name_of_schema_file = '../datamodel/genap-genepanel-config-schema.json'
+    abs_filename = os.path.join(os.path.dirname(__file__), name_of_schema_file)
 
-    with open(filename) as schema_file:
+    with open(abs_filename) as schema_file:
         my_schema = json.load(schema_file)
         # see doc http://python-jsonschema.readthedocs.io/en/latest/validate/#validating-formats
         validate(config, my_schema, format_checker=FormatChecker())
