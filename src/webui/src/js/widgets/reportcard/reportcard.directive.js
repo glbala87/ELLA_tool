fe8f5393ad77f887d78eaf25e266cc1f2e5b9ea4
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-08-11 12:05:09 +0200

    [webui] Add toggle functionality in sidebar for report view

--- src/webui/src/js/widgets/reportcard/reportcard.directive.js
+++ src/webui/src/js/widgets/reportcard/reportcard.directive.js
@@ -1,6 +1,7 @@
 /* jshint esnext: true */
 
 import {Directive, Inject} from '../../ng-decorators';
+import {AlleleStateHelper} from '../../model/allelestatehelper';
 
 @Directive({
     selector: 'report-card',
@@ -35,69 +36,24 @@ export class ReportCardController {
         this.selected_excluded = null;
     }
 
-    getIncludedAlleles() {
-        let included = this.alleles.filter(allele => {
-            let allele_state = this.getAlleleState(allele);
-            if (allele_state &&
-                'report' in allele_state &&
-                'included' in allele_state.report) {
-                return allele_state.report.included;
-            }
-            return false;
-        });
-        included.sort((a, b) => {
-            let ac = this.getClassification(a);
-            let bc = this.getClassification(b);
-            let aci = this.config.classification.options.findIndex(elem => {
-                return elem.value === ac;
-            });
-            let bci = this.config.classification.options.findIndex(elem => {
-                return elem.value === bc;
-            });
-            return bci - aci;
-        });
-        return included;
-    }
-
-    getExcludedAlleles() {
-        return this.alleles.filter(allele => {
-            let allele_state = this.state.allele.find(al => al.allele_id === allele.id);
-            if (allele_state &&
-                'report' in allele_state &&
-                'included' in allele_state.report) {
-                return !allele_state.report.included;
-            }
-            return true;
-        });
-    }
-
-    getIncludedNeedsVerification() {
-        return this.getIncludedAlleles().filter(al => {
-            return al.annotation.quality.needs_verification;
-        });
-    }
-
-    includeSelectedExcluded() {
-        if (this.selected_excluded) {
-            let allele_state = this.getAlleleState(this.selected_excluded);
-            if (!('report' in allele_state)) {
-                allele_state.report = {};
-            };
-            allele_state.report.included = true;
-        }
-        this.selected_excluded = null;
-    }
-
     getAlleleState(allele) {
         return this.state.allele.find(al => al.allele_id === allele.id);
     }
 
     getClassification(allele) {
-        let allele_state = this.getAlleleState(allele);
-        if ('alleleassessment' in allele_state &&
-            'classification' in allele_state.alleleassessment) {
-            return allele_state.alleleassessment.classification;
-        }
+        return AlleleStateHelper.getClassification(allele, this.getAlleleState(allele));
+    }
+
+    getAlleles() {
+        this.alleles.sort(
+            firstBy(a => {
+                let classification = AlleleStateHelper.getClassification(a, this.getAlleleState(a));
+                return this.config.classification.options.findIndex(o => o.value === classification);
+            }, -1)
+            .thenBy(a => a.annotation.filtered[0].SYMBOL)
+            .thenBy(a => a.annotation.filtered[0].HGVSc_short)
+        )
+        return this.alleles;
     }
 
     formatHGVS(allele) {
