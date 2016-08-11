93d0b61167c9f7ee1ebdcdafc9152aaffc36c374
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-08-11 12:05:09 +0200

    [webui] Add toggle functionality in sidebar for report view

--- src/webui/src/js/views/alleleSidebar.directive.js
+++ src/webui/src/js/views/alleleSidebar.directive.js
@@ -7,8 +7,8 @@ import {AlleleStateHelper} from '../model/allelestatehelper';
     selector: 'allele-sidebar',
     templateUrl: 'ngtmpl/alleleSidebar.ngtmpl.html',
     scope: {
-        alleles: '=',  // Allele options: { unclassified: [ {allele: Allele, alleleState: {...}, inactive: true ] }, classified: [ ... ] }
-        selected: '=' // Selected Allele
+        alleles: '=',  // Allele options: { unclassified: [ {allele: Allele, alleleState: {...}, inactive: true, checkable: true, checked: true ] }, classified: [ ... ] }
+        selected: '=', // Selected Allele
     }
 })
 @Inject()
@@ -17,12 +17,38 @@ export class AlleleSidebarController {
     constructor() {
     }
 
-    select(allele) {
-        this.selected = allele;
+    select(allele_option) {
+        // We have two modes, multiple checkable or normal radio selectiion (of single allele)
+
+        // Multiple (if 'checkable' === true)
+        if (this.isTogglable(allele_option)) {
+            allele_option.toggle();
+        }
+        // Single selection
+        else {
+            this.selected = allele_option.allele;
+        }
     }
 
-    isSelected(allele) {
-        return this.selected === allele;
+    isSelected(allele_option) {
+        let selected = this.selected === allele_option.allele;
+        // If checkable is true, we don't support select mode. Set to null
+        if (selected && allele_option.checkable) {
+            this.selected = null;
+            return false;
+        }
+        return selected;
+    }
+
+    isToggled(allele_option) {
+        if (this.isTogglable(allele_option)) {
+            return allele_option.isToggled();
+        }
+        return false;
+    }
+
+    isTogglable(allele_option) {
+        return allele_option.togglable;
     }
 
     getClassification(allele, allele_state) {
