1e8416461cd38fc0652c09cae90b7a338821e948
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2016-11-11 15:41:23 +0100

    [api] Remove unused parts of allele schema

--- src/api/schemas/alleles.py
+++ src/api/schemas/alleles.py
@@ -3,18 +3,12 @@ from marshmallow import Schema
 
 class AlleleSchema(Schema):
 
-    def __init__(self, include_annotation=False):
-        super(AlleleSchema, self).__init__()
-
-        if include_annotation:
-            self.Meta.fields.append('annotation')
-
     class Meta:
-        fields = ['id',
+        fields = ('id',
                   'genome_reference',
                   'chromosome',
                   'start_position',
                   'open_end_position',
                   'change_from',
                   'change_to',
-                  'change_type']
+                  'change_type')
