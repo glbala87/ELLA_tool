2e85909dd7b441ceedc640207cf438d4edc046e5
monoburn <erik.severinsen@medisin.uio.no>
2016-11-21 21:00:25 +0100

    [test] added test for filter noUnderscores

--- src/webui/tests/unit/filters.spec.js
+++ src/webui/tests/unit/filters.spec.js
@@ -97,6 +97,27 @@ describe("filter", function () {
 
     });
 
+    describe("noUnderscores", function () {
+        var f;
+
+        beforeEach(function () {
+            angular.mock.module('workbench');
+            inject(
+                function (_$filter_) {
+                    f = _$filter_('noUnderscores');
+                });
+        });
+
+        it("replaces _ with space", function() {
+            expect(f('abc_def')).toEqual('abc def');
+        });
+
+        it("returns empty string for undefined", function() {
+            expect(f(undefined)).toEqual("");
+        });
+
+    });
+
+
 
 });
-
