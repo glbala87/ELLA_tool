bcefcc5024782d87c96f22b32cd7a4b99ea5bfcf
Øyvind Evju <oyvind.evju@medisin.uio.no>
2016-11-21 15:42:27 +0100

    Add polling of annotation server.
    Monitors annotation servers, and tries to deposit any annotation reported back as successful.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,11 +1,63 @@
 from flask import Flask
 from werkzeug.routing import BaseConverter
+from werkzeug.serving import is_running_from_reloader
 
 from vardb.datamodel import DB
 from rest_query import RestQuery
 
-app = Flask(__name__)
+import threading
+import atexit
+import time
+import json
 
+def create_app():
+    app = Flask(__name__)
+
+    pollAnnotationServiceThread = threading.Thread()
+
+    def polling(app, event):
+        while not event.is_set():
+            with app.test_client() as c:
+                # Get status of all submitted tasks
+                response = c.get("/api/v1/analyses/imports/")
+                if response.status_code == 200:
+                    status = json.loads(response.get_data())
+                    for id in status:
+                        # Try to deposit annotated vcf, if successful
+                        if status[id] == "SUCCESS":
+                            MAX_TRIES = 3
+                            WAIT = 5
+                            try_no = 0
+                            while True:
+                                try_no += 1
+                                try:
+                                    process = c.post("/api/v1/analyses/imports/%s" %id)
+                                    break
+                                except:
+                                    time.sleep(WAIT)
+                                    if try_no >= MAX_TRIES:
+                                        break
+                            c.delete("/api/v1/analyses/imports/%s" % id)
+            time.sleep(5)
+        print "Received exit signal"
+
+    def startPolling(app):
+        global pollAnnotationServiceThread
+        # Create event for cancelling thread
+        event = threading.Event()
+        pollAnnotationServiceThread = threading.Thread(target=polling, args=(app,event))
+        pollAnnotationServiceThread.start()
+
+        return event.set
+
+    # Initiate
+    if not is_running_from_reloader():
+        cancel = startPolling(app)
+        atexit.register(cancel)
+    return app
+
+# Setup app, and create polling thread
+app = create_app()
 
 class ListConverter(BaseConverter):
     def to_python(self, value):
