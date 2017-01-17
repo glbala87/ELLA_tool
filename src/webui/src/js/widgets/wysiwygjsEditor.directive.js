e7282e96b6681cc61ad2b6da02f7fe8020131c08
Øyvind Evju <oyvind.evju@medisin.uio.no>
2017-01-17 11:15:04 +0100

    [ui] Add link button. Make link form cross-browser compatible.

--- src/webui/src/js/widgets/wysiwygjsEditor.directive.js
+++ src/webui/src/js/widgets/wysiwygjsEditor.directive.js
@@ -104,13 +104,14 @@ function getCurrentColors(nodes) {
                 '<button class="wysiwygbutton" title="Normal text" id="wysiwyg-paragraph">P</button>' +
                 '<button class="wysiwygbutton" title="Bullet list" id="wysiwyg-orderedList"><svg width="60%" height="100%" fill="currentcolor" stroke="currentcolor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M381 1620q0 80-54.5 126t-135.5 46q-106 0-172-66l57-88q49 45 106 45 29 0 50.5-14.5t21.5-42.5q0-64-105-56l-26-56q8-10 32.5-43.5t42.5-54 37-38.5v-1q-16 0-48.5 1t-48.5 1v53h-106v-152h333v88l-95 115q51 12 81 49t30 88zm2-627v159h-362q-6-36-6-54 0-51 23.5-93t56.5-68 66-47.5 56.5-43.5 23.5-45q0-25-14.5-38.5t-39.5-13.5q-46 0-81 58l-85-59q24-51 71.5-79.5t105.5-28.5q73 0 123 41.5t50 112.5q0 50-34 91.5t-75 64.5-75.5 50.5-35.5 52.5h127v-60h105zm1409 319v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-14 9-23t23-9h1216q13 0 22.5 9.5t9.5 22.5zm-1408-899v99h-335v-99h107q0-41 .5-122t.5-121v-12h-2q-8 17-50 54l-71-76 136-127h106v404h108zm1408 387v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-14 9-23t23-9h1216q13 0 22.5 9.5t9.5 22.5zm0-512v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5z"/></svg></button> ' +
                 '<button class="wysiwygbutton" title="Numbered list" id="wysiwyg-unorderedList"><svg width="60%" height="100%" fill="currentcolor" stroke="currentcolor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path class="path1" d="M384 1408q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm0-512q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm1408 416v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5zm-1408-928q0 80-56 136t-136 56-136-56-56-136 56-136 136-56 136 56 56 136zm1408 416v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5zm0-512v192q0 13-9.5 22.5t-22.5 9.5h-1216q-13 0-22.5-9.5t-9.5-22.5v-192q0-13 9.5-22.5t22.5-9.5h1216q13 0 22.5 9.5t9.5 22.5z"></path></svg></button>' +
-                '<button class="wysiwygbutton" title="Add link" id="wysiwyg-link"><svg width="70%" height="100%" id="i-link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%">   <path d="M18 8 C18 8 24 2 27 5 30 8 29 12 24 16 19 20 16 21 14 17 M14 24 C14 24 8 30 5 27 2 24 3 20 8 16 13 12 16 11 18 15" /></svg>' +
-                    '<div class="wysiwyglinkform" tabindex="1" hidden>' +
-                        '<div><input id="wysiwygklinkurl" placeholder="LINK ADDRESS"></div>' +
-                        '<div><input id="wysiwygklinktext" placeholder="(OPTIONAL) TEXT"></div>' +
-                    '</div>' +
-                '</button>' +
+                '<button class="wysiwygbutton" title="Add link" id="wysiwyg-link"><svg width="70%" height="100%" id="i-link" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%">   <path d="M18 8 C18 8 24 2 27 5 30 8 29 12 24 16 19 20 16 21 14 17 M14 24 C14 24 8 30 5 27 2 24 3 20 8 16 13 12 16 11 18 15" /></svg></button>' +
                 '<button class="wysiwygbutton" title="Clear formatting" id="wysiwyg-removeFormat">T<span style="vertical-align: sub; margin-left: -0.3rem">&#10005</span></button>' +
+                '<div class="wysiwyglinkform" tabindex="1" hidden>' +
+                    '<div><input id="wysiwygklinkurl" placeholder="LINK ADDRESS"></div>' +
+                    '<div><input id="wysiwygklinktext" placeholder="(OPTIONAL) TEXT"></div>' +
+                    '<button class="addlinkbutton"><svg id="i-checkmark" viewBox="0 0 32 32" width="100%" height="100%" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6.25%"> <path d="M2 20 L12 28 30 4" /></svg></button>' +
+                    // '<button class="addlinkbutton">&#10004;</button>' +
+                '</div>' +
               '</div> ',
     link: (scope, elem, attrs, ngModel) => {
         let editorelement = elem.children()[0];
@@ -120,7 +121,7 @@ function getCurrentColors(nodes) {
 
         buttonselement.hidden = true;
         let buttons = {};
-        for (let i=0; i<buttonselement.children.length; i++) {
+        for (let i=0; i<buttonselement.children.length-1; i++) {
             let button = buttonselement.children[i];
             let name = button.id.split('-')[1];
             eventListeners.add(button, "mousedown", function () {scope.vm.blurBlocked = true;});
@@ -196,6 +197,7 @@ function getCurrentColors(nodes) {
 
         function getTextFromHTML(html) {
             html = html.replace(/<\/?(br|ul|ol|strong|em|li|h1|h2|h3|h4|p|div)[^>]*>/g, '');
+            html = html.replace('/s+/g', '');
             return html;
         }
 
@@ -205,13 +207,14 @@ function getCurrentColors(nodes) {
                     editor.setHTML("")
                     placeholderEvent(true);
                 }
+                closeLinkForm(true);
                 buttonselement.hidden = true;
-                closeLinkForm(true, null);
             }
         }
 
         function focus() {
             placeholderEvent(false);
+            closeLinkForm(true);
             editorelement.focus();
             buttonselement.hidden = false;
         }
@@ -263,23 +266,36 @@ function getCurrentColors(nodes) {
 
         // Add eventhandlers on link-form
         eventListeners.add(buttons["link"], "click", handleLinkForm);
-        let linkinputs = buttons["link"].getElementsByTagName("input");
+        // eventListeners.add(buttons["link"], "blur", () => {setTimeout(closeLinkForm, 1, false)}); // Close if active element not in link form));
+        var linkform = buttonselement.children[buttonselement.children.length-1];
+        eventListeners.add(linkform, "blur", () => {setTimeout(closeLinkForm, 1, false)}); // Close if active element not in link form)
+        let linkinputs = linkform.getElementsByTagName("input");
+        let addbutton = linkform.getElementsByTagName("button")[0];
+        eventListeners.add(addbutton, "click", addLink);
         for (let i=0; i<linkinputs.length; i++) {
             eventListeners.add(linkinputs[i], "blur", () => {setTimeout(closeLinkForm, 1, false)}); // Close if active element not in link form
             eventListeners.add(linkinputs[i], "keyup", handleLinkForm); // Only handles esc and enter
         }
-        var linkform = buttons["link"].children[1];
-
 
         // Handle link form
         function handleLinkForm(e) {
             let src = e.target || e.srcElement;
+            console.log(src)
+
             if (src.nodeName !== "INPUT" && (src === buttons["link"] || buttons["link"].contains(src))) {
                 // Open or close link form
                 if (linkform.hidden) {
                     editor.openPopup(); // Save selection, to later add link at right location
+                    // Compute position
+                    let top = buttons["link"].offsetTop;
+                    let height = buttons["link"].offsetHeight;
+                    let left = buttons["link"].offsetLeft;
+                    linkform.style.left = left+"px";
+                    linkform.style.top = (top+height)+"px";
+
                     linkform.hidden = false;
                 } else {
+                    console.log("Closing link form")
                     closeLinkForm(true)
                 }
 
@@ -290,27 +306,37 @@ function getCurrentColors(nodes) {
                     if (e.key === "Escape") {
                         closeLinkForm(true)
                     } else if (e.key === "Enter") {
-
-                        // Get url
-                        let url = linkform.children[0].children[0].value;
-                        if (!url.startsWith("http")) { // Should start with either http or https
-                            url = "http://"+url;
-                        }
-
-                        // Get link text
-                        let linktext = linkform.children[1].children[0].value;
-                        linktext = linktext !== "" ? linktext: url;
-
-                        // Insert link
-                        editor.insertHTML('<div><a href="'+url+'" target="'+url+'"><span>'+linktext+'</span></a></div>');
-                        closeLinkForm(true)
+                        addLink()
                     }
                 }
             }
         }
 
+        function addLink() {
+            // Get url
+            let url = linkform.children[0].children[0].value;
+
+            if (url.replace(/\s+/g, '') === "") {
+                // Empty url
+                closeLinkForm(true);
+                return;
+            }
+            if (!url.startsWith("http")) { // Should start with either http or https
+                url = "http://"+url;
+            }
+
+            // Get link text
+            let linktext = linkform.children[1].children[0].value;
+            linktext = linktext !== "" ? linktext: url;
+
+            // Insert link
+            editor.insertHTML('<div><a href="'+url+'" target="'+url+'"><span>'+linktext+'</span></a></div>');
+            closeLinkForm(true)
+        }
+
         function closeLinkForm(force) {
-            if (!force && buttons["link"].contains(document.activeElement)) {
+            // if (!force && buttons["link"].contains(document.activeElement)) {
+            if (!force && linkform.contains(document.activeElement)) {
                 // Link form is still active
                 return;
             }
@@ -324,12 +350,8 @@ function getCurrentColors(nodes) {
 
             editor.closePopup().collapseSelection()
             scope.vm.blurBlocked = false;
+            editorelement.focus();
         }
-
-
-
-
-
     }
 })
 
