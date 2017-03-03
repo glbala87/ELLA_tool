d9510615a3356b143958373881468c1d3365216d
Øyvind Evju <oyvind.evju@medisin.uio.no>
2017-03-03 15:05:54 +0100

    [tests] Move test file for single sample deposit

---
+++ src/vardb/deposit/tests/test_deposit_single.py
@@ -0,0 +1,83 @@
+import json
+import pytest
+import os
+from vardb.deposit.deposit_analysis import DepositAnalysis
+from vardb.datamodel import genotype, allele, sample
+
+
+## HELPER FUNCTIONS
+
+def get_genotype(genotypes, first_change, second_change, _ret=[]):
+    gts = []
+    for gt in genotypes:
+        if second_change is not None and gt.secondallele is None:
+            continue
+        elif second_change is None and gt.secondallele is None:
+            if gt.allele.change_type == first_change:
+                gts.append(gt)
+        elif set([gt.allele.change_type, gt.secondallele.change_type]) == set([first_change, second_change]):
+            gts.append(gt)
+    return gts
+
+
+
+## FIXTURES
+
+@pytest.fixture(scope="module", autouse=True)
+def deposit_single(session):
+    """Deposit test analysis"""
+    single = "../../testdata/analyses/all/brca_decomposed.HBOC_v01"
+    files = os.listdir(single)
+    assert len(files) == 3
+    sample_configs_file = os.path.join(single, [f for f in files if f.endswith(".sample")][0])
+    analysis_config_file = os.path.join(single, [f for f in files if f.endswith(".analysis")][0])
+    vcf_file = os.path.join(single, [f for f in files if f.endswith(".vcf")][0])
+    sample_configs = [json.load(open(sample_configs_file, 'r'))]
+    analysis_config = json.load(open(analysis_config_file, 'r'))
+
+    deposit_analysis = DepositAnalysis(session)
+    deposit_analysis.import_vcf(vcf_file, sample_configs=sample_configs, analysis_config=analysis_config)
+
+@pytest.fixture(scope="module")
+def all_genotypes_single(session):
+    """return all genotypes imported in this analysis"""
+    analysis_name = "brca_decomposed.HBOC_v01"
+    all_genotypes = session.query(genotype.Genotype).join(
+        sample.Analysis
+    ).filter(
+        sample.Analysis.name == analysis_name,
+    ).all()
+    return all_genotypes
+
+
+## TESTS
+
+# There should be one heterozygous SNP, del, and ins (singleallelic)
+@pytest.mark.parametrize("variant_type", ("SNP", "ins", "del"))
+def test_single_singlealleic(all_genotypes_single, variant_type):
+    gts = get_genotype(all_genotypes_single, variant_type, None)
+    assert len(gts) == 1
+    gt = gts[0]
+    assert gt.secondallele is None
+    assert "REF" in gt.allele_depth
+    assert len(gt.allele_depth) == 2
+    assert gt.allele.vcf_alt in gt.allele_depth
+
+
+@pytest.mark.parametrize("variant_types", (
+        ("SNP","SNP"),
+        ("SNP","ins"),
+        ("SNP","del"),
+        ("ins","del")
+    ))
+def test_single_multiallelic(all_genotypes_single, variant_types):
+    # There should be one heterozygous SNP,SNP (multiallelic)
+    gts = get_genotype(all_genotypes_single, *variant_types)
+    assert len(gts) == 1
+    gt = gts[0]
+    assert gt.allele.id != gt.secondallele.id
+    assert gt.allele.vcf_pos == gt.secondallele.vcf_pos
+    assert "REF" in gt.allele_depth
+    assert len(gt.allele_depth) == 3
+    assert gt.allele.vcf_alt in gt.allele_depth
+    assert gt.secondallele.vcf_alt in gt.allele_depth
