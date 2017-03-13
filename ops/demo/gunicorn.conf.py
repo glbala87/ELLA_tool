63a29ca4ebfe9f9c3f02542bc17ddf1fb5999539
monoburn <monoburn@gmail.com>
2017-03-13 14:42:28 +0100

    [ops] sharing of env specific files

--- ops/demo/gunicorn.conf.py
+++ ops/demo/gunicorn.conf.py
@@ -1,1 +1,9 @@
-../prod/gunicorn.conf.py
+import multiprocessing
+
+bind = 'unix:/socket/api.sock'
+backlog = 1024
+workers = multiprocessing.cpu_count() * 2 + 1
+daemon = False
+loglevel = 'debug'
+errorlog = '-'
+accesslog = '-'
