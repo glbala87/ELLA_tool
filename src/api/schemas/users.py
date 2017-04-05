53d9e4b8bf1345551c70648dd8c81dfb53934e1a
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2017-04-05 17:03:55 +0200

    [api] Fix collision detection: Filter alleles and remove alleleassessment check

--- src/api/schemas/users.py
+++ src/api/schemas/users.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema
+from marshmallow import Schema, fields
 
 
 class UserSchema(Schema):
@@ -6,4 +6,15 @@ class UserSchema(Schema):
         fields = ('id',
                   'username',
                   'first_name',
-                  'last_name')
+                  'last_name',
+                  'full_name',
+                  'abbrev_name')
+
+    full_name = fields.Method('get_full_name')
+    abbrev_name = fields.Method('get_abbreviated_name')
+
+    def get_full_name(self, obj):
+        return ' '.join([obj.first_name, obj.last_name])
+
+    def get_abbreviated_name(self, obj):
+        return '{}. {}'.format(obj.first_name.substring(0, 1), obj.last_name)
