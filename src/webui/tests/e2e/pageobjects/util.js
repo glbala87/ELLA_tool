87dd00cf2ab53a4bbaf5fe8925080dc89db1ad5a
monoburn <erik.severinsen@medisin.uio.no>
2017-09-14 12:40:11 +0200

    Override console.log to include timestamp

--- src/webui/tests/e2e/pageobjects/util.js
+++ src/webui/tests/e2e/pageobjects/util.js
@@ -1,5 +1,31 @@
 const DEBUG = process.env.DEBUG;
 
+var log = console.log;
+
+console.log = function () {
+    var first_parameter = arguments[0];
+    var other_parameters = Array.prototype.slice.call(arguments, 1);
+
+    function formatConsoleDate (date) {
+        var hour = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        var milliseconds = date.getMilliseconds();
+
+        return '[' +
+               ((hour < 10) ? '0' + hour: hour) +
+               ':' +
+               ((minutes < 10) ? '0' + minutes: minutes) +
+               ':' +
+               ((seconds < 10) ? '0' + seconds: seconds) +
+               '.' +
+               ('00' + milliseconds).slice(-3) +
+               '] ';
+    }
+
+    log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(other_parameters));
+};
+
 /**
  * Utilities for logging and working with the WebdriverIO API.
  */
