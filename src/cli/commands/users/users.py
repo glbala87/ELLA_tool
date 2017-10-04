6c2d4e2d22391a2906220cd9caa901c0e0027e43
Erik Severinsen <monoburn@gmail.com>
2017-10-04 13:26:05 +0200

    [ci skip] add a dry option to shows users that would be imported

--- src/cli/commands/users/users.py
+++ src/cli/commands/users/users.py
@@ -127,6 +127,7 @@ def cmd_users_list():
     for u in users:
         click.echo(row_format.format(**{h: encode(getattr(u,h)) for h in header}))
 
+
 @users.command('activity')
 def cmd_users_activity():
     """
@@ -151,7 +152,6 @@ def _add_user(session, username, first_name, last_name, usergroup):
     """
     Add user with a generated password
     """
-
 
     existing_user = session.query(user.User).filter(
         user.User.username == username,
@@ -211,13 +211,9 @@ def cmd_add_user(username, first_name, last_name, usergroup):
 @click.argument("json_file")
 @click.option('--group', multiple=True, help="Limit the import to users belonging to specific usergroups, multiple options allowed." \
                 + "If 'ALL' is given as option all users are imported")
-def cmd_add_many_users(json_file, group):  # group is a tuple of names given as --group options
+@click.option('-dry', is_flag=True, help="List users that would be imported")
+def cmd_add_many_users(json_file, group, dry):  # group is a tuple of names given as --group options
     from functools import partial
-
-    db = DB()
-    db.connect()
-    session = db.session()
-
     users = json.load(open(json_file, 'r'))
 
     def is_usergroup_configured_to_be_imported(group_names, user):
@@ -227,6 +223,21 @@ def cmd_add_many_users(json_file, group):  # group is a tuple of names given as 
                and user["usergroup"].strip().lower() in map(lambda s: s.strip().lower(), group_names)
 
     filtered_users = users if 'ALL' in group else filter(partial(is_usergroup_configured_to_be_imported, group), users)
+
+    if dry:
+        for u in filtered_users:
+            click.echo(u"Would add user '{username}' ('{last_name}', '{first_name}') from '{usergroup}')".format(
+                username=u["username"],
+                first_name=u["first_name"],
+                last_name=u["last_name"],
+                usergroup=u["usergroup"]
+            ))
+        return
+
+    db = DB()
+    db.connect()
+    session = db.session()
+
     for u in filtered_users:
         try:
             u, pw = _add_user(session, u["username"], u["first_name"], u["last_name"], u["usergroup"])
@@ -321,15 +332,15 @@ def cmd_modify_user(username, **kwargs):
         last_name=u_after.last_name,
     ))
 
-    N_changes = 0
+    n_changes = 0
     for k in modified:
         from_val = encode(getattr(u_before, k))
         to_val = encode(getattr(u_after, k))
         if from_val != to_val:
-            N_changes += 1
+            n_changes += 1
             click.echo("\t{key}: {from_val} ---> {to_val}".format(key=k, from_val=from_val, to_val=to_val))
 
-    if N_changes == 0:
+    if n_changes == 0:
         click.echo("No modifications made!")
 
 
