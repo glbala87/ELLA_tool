1829344aa7641dd51b8ea09ca6257ee1fb35cd15
scalavision <scalavision@gmail.com>
2017-10-06 09:42:32 +0200

    wip - added backend support for showing users group, other users in group and gene panels, next up is frontend

--- src/api/schemas/users.py
+++ src/api/schemas/users.py
@@ -21,11 +21,21 @@ class UserSchema(Schema):
     def get_abbreviated_name(self, obj):
         return u" ".join([v[0]+"." for v in [obj.first_name]+obj.last_name.split()[:-1]])+" "+obj.last_name.split()[-1]
 
+class UserGroupGenepanelSchema(Schema):
+    class Meta:
+        fields = (
+          'name',
+          'version',
+          'genome_reference'
+        )
+
 class UserGroupSchema(Schema):
     class Meta:
         fields = ( 'id',
-                   'name'     
+                   'name',
+                   'genepanels'
         )
+    genepanels = fields.Nested(UserGroupGenepanelSchema, many=True)
 
 class UserFullSchema(Schema):
     class Meta:
@@ -37,12 +47,14 @@ class UserFullSchema(Schema):
                   'abbrev_name',
                   'active',
                   'password_expiry',
-                  'group'
+                  'group',
+                  'otherUsersInGroup'
         )
 
     full_name = fields.Method('get_full_name')
     abbrev_name = fields.Method('get_abbreviated_name')
     group = fields.Nested(UserGroupSchema)
+    otherUsersInGroup = fields.Nested('self',exclude=('otherUsersInGroup'), only=['first_name', 'last_name', 'username'], many=True)
 
     def get_full_name(self, obj):
         return ' '.join([obj.first_name, obj.last_name])
