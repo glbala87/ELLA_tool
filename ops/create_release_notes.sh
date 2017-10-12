24230ffba42c842f4d8531e10f5432287b530e3d
monoburn <monoburn@gmail.com>
2017-10-12 15:26:16 +0200

    added comment about 'git log'

--- ops/create_release_notes.sh
+++ ops/create_release_notes.sh
@@ -16,5 +16,6 @@ echo "$BLANK_LINE" >> $file
 git tag --list $from $to --format='%(objecttype) %(refname:strip=2) (%(objectname:short)) "%(contents:subject)" created on %(creatordate:iso-strict) by %(creator)' >> $file
 echo "$SEP" >> $file
 echo "$BLANK_LINE" >> $file
+# Only look at commits from merge requests (not 100%, will also pick up merges into feature branches)
 git log --merges "$from..$to" >> $file
 
