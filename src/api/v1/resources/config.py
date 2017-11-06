b7bd2a0e2f9fb9aea93c4026b3ae82541f8c82fc
monoburn <monoburn@gmail.com>
2017-11-06 16:22:37 +0100

    rename parameters and handle None

--- src/api/v1/resources/config.py
+++ src/api/v1/resources/config.py
@@ -6,21 +6,23 @@ from vardb.datamodel import user
 import collections
 
 # https://gist.github.com/angstwad/bf22d1822c38a92ec0a9
-def dict_merge(dct, merge_dct):
+def dict_merge(destination, src):
     """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
     updating only top-level keys, dict_merge recurses down into dicts nested
     to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
     ``dct``.
-    :param dct: dict onto which the merge is executed
-    :param merge_dct: dct merged into dct
+    :param destination: dict onto which the merge is executed
+    :param src: dct merged into dct
     :return: None
     """
-    for k, v in merge_dct.iteritems():
-        if (k in dct and isinstance(dct[k], dict)
-                and isinstance(merge_dct[k], collections.Mapping)):
-            dict_merge(dct[k], merge_dct[k])
+    if not src:
+        return
+    for k, v in src.iteritems():
+        if (k in destination and isinstance(destination[k], dict)
+                and isinstance(src[k], collections.Mapping)):
+            dict_merge(destination[k], src[k])
         else:
-            dct[k] = merge_dct[k]
+            destination[k] = src[k]
 
 class ConfigResource(LogRequestResource):
     @authenticate(optional=True)
