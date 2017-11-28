b3860d6d43c0404aeb911b1325d96b8b2743d152
Erik Severinsen <monoburn@gmail.com>
2017-11-28 07:48:33 +0100

    [ci skip] init sanger export

--- src/cli/commands/export/export.py
+++ src/cli/commands/export/export.py
@@ -33,3 +33,23 @@ def cmd_export_classifications(filename):
     db.connect()
     dump_classification.dump_alleleassessments(db.session, output_name)
     click.echo("Exported variants to " + output_name + '.xls/csv')
+
+@export.command('sanger', help="Export variants that needs to be Sanger verified")
+@click.option('--filename', help="The name of the file to create. Suffix .xls and .csv will be automatically added.\n" 
+                                  "Default: 'variant-sanger-YYYY-MM-DD_hhmm.xls/csv'")
+def cmd_export_classifications(filename):
+    """
+    Exports all current classifications into an excel file.
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    today = datetime.datetime.now()
+    timestamp = today.strftime(FILENAME_TIMESTAMP_FORMAT)
+    output_name = filename if filename else "variant-sanger-{timestamp}".format(timestamp=timestamp)
+
+    # return
+
+    # db = DB()
+    # db.connect()
+    # dump_classification.dump_alleleassessments(db.session, output_name)
+    click.echo("Exported variants to " + output_name + '.xls/csv')
