0d0baff41b107c042977642180cc0429537b77bd
monoburn <monoburn@gmail.com>
2017-12-06 15:58:05 +0100

    fixed filename

--- ops/test/report-sanger/run-test-report-is-empty.sh
+++ ops/test/report-sanger/run-test-report-is-empty.sh
@@ -8,7 +8,7 @@ while ! pg_isready --dbname=postgres --username=postgres; do sleep 2; done
 echo "Starting report tests..."
 ./ella-cli export sanger --filename e2e-sanger-export
 grep "file is intentionally empty" e2e-sanger-export.csv
-linecount = $(wc -l e2e-sanger-export)
+linecount = $(wc -l e2e-sanger-export.csv)
 if [ "$linecount" -ne 1 ]
 then
 	echo "File should have single line"; exit 1)
