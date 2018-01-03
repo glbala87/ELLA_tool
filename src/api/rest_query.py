c7d04c5785a55dff80434def5026471eb1fb36de
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-01-03 13:30:27 +0100

    [api] Remove unused code

--- src/api/rest_query.py
+++ src/api/rest_query.py
@@ -27,46 +27,7 @@ def pg_explain(element, compiler, **kw):
     return text
 
 
-OPERATORS = {
-    # Operators which accept two arguments.
-    '$eq': lambda f, a: f == a,
-    '$neq': lambda f, a: f != a,
-    '$gt': lambda f, a: f > a,
-    '$lt': lambda f, a: f < a,
-    '$gte': lambda f, a: f >= a,
-    '$lte': lambda f, a: f <= a,
-    '$in': lambda f, a: f.in_(a),
-    '$nin': lambda f, a: ~f.in_(a),
-    '$like': lambda f, a: f.op('~')(a),
-    '$ilike': lambda f, a: f.op('~*')(a),
-}
-
-
 class RestQuery(Query):
-
-    def rest_filter(self, q):
-        raise RuntimeError("Code should be dead?")
-        # based on filter_by() from sqlalchemy source
-
-        clauses = list()
-
-        for k, v in q.iteritems():
-            if isinstance(v, list):
-                if v:  # Asking for empty list doesn't make sense
-                    if isinstance(k, tuple):
-                        args.append(tuple_(*(getattr(model, _k) for _k in k)).in_(v))
-                    else:
-                        args.append(getattr(model, k).in_(v))
-            # field = _entity_descriptor(self._joinpoint_zero(), k)
-            elif isinstance(v, dict):
-                for op_k, op_v in v.iteritems():
-                    if op_k in OPERATORS:
-                        clauses.append(OPERATORS[op_k](field, op_v))
-            else:
-                # Default is '=='
-                clauses.append(field == v)
-
-        return self.filter(sql.and_(*clauses))
 
     def explain(self, analyze=False, print_json=False, stdout=True):
         """
