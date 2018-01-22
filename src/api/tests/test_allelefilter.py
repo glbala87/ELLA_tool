f47af7ccf18ae29f85fa8a1ae92279abb6ab77bb
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-01-22 17:41:18 +0100

    [api] Work with sets in allelefilter to avoid duplicates

--- src/api/tests/test_allelefilter.py
+++ src/api/tests/test_allelefilter.py
@@ -604,7 +604,7 @@ class TestAlleleFilter(object):
         allele_info = [a2common.id]
         result = af.get_commonness_groups({gp_key: allele_info})
 
-        assert result[gp_key]['common'] == [a2common.id]
+        assert result[gp_key]['common'] == set([a2common.id])
         assert not result[gp_key]['less_common']
         assert not result[gp_key]['low_freq']
         assert not result[gp_key]['null_freq']
@@ -640,7 +640,7 @@ class TestAlleleFilter(object):
         result = af.get_commonness_groups({gp_key: allele_info})
 
         assert not result[gp_key]['common']
-        assert result[gp_key]['less_common'] == [a2less_common.id]
+        assert result[gp_key]['less_common'] == set([a2less_common.id])
         assert not result[gp_key]['low_freq']
         assert not result[gp_key]['null_freq']
 
@@ -672,7 +672,7 @@ class TestAlleleFilter(object):
 
         assert not (result[gp_key]['common'])
         assert not result[gp_key]['less_common']
-        assert result[gp_key]['low_freq'] == [a2low_freq.id]
+        assert result[gp_key]['low_freq'] == set([a2low_freq.id])
         assert not result[gp_key]['null_freq']
 
     @pytest.mark.aa(order=2)
