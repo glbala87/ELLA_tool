1d1aaaa614a99477809a1f11d268ad0db8994425
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-02-14 10:14:31 +0100

    [watcher] Fix tests

--- src/vardb/watcher/tests/test_analysis_watcher.py
+++ src/vardb/watcher/tests/test_analysis_watcher.py
@@ -46,13 +46,13 @@ def test_analysispath_throws_exception(session):
         AnalysisWatcher(session, non_existing_path, '')
     assert WATCH_PATH_ERROR.format(non_existing_path) in str(excinfo)
 
- 
+
 def test_destinationpath_throws_exception(session):
     with pytest.raises(RuntimeError) as excinfo:
         AnalysisWatcher(session, watch_path, non_existing_path)
-    assert DEST_PATH_ERROR.format(non_existing_path) in str(excinfo) 
+    assert DEST_PATH_ERROR.format(non_existing_path) in str(excinfo)
 
-  
+
 def test_ready_filepath(session, init_dest):
     aw = init(session)
     assert aw.is_ready(ready_data_path) == True
@@ -65,23 +65,23 @@ def test_path_to_analysis_config(session, init_dest):
     # Name of .analysis file should match dir name
     #'/ella/src/vardb/watcher/testdata/analyses/TestAnalysis-001/TestAnalysis-001.analysis'
     assert analysis_config_path == ready_data_path + '/' + analysis_sample + ANALYSIS_POSTFIX
-  
+
     with pytest.raises(RuntimeError) as excinfo:
-        aw.path_to_analysis_config(empty_data_path, analysis_sample) 
-    
+        aw.path_to_analysis_config(empty_data_path, analysis_sample)
+
     expected_error = ANALYSIS_FILE_MISSING.format(empty_data_path + '/' + analysis_sample + ANALYSIS_POSTFIX)
-    assert expected_error  in str(excinfo)
+    assert expected_error in str(excinfo)
 
 
 def test_loading_report(session, init_dest):
     aw = init(session)
-    report = aw.load_file(ready_data_path, REPORT_FILE)
+    report = aw.load_file(ready_data_path, REPORT_FILES[0])
     assert "Report" in str(report)
 
 
-def test_loading_report(session, init_dest):
+def test_loading_warnings(session, init_dest):
     aw = init(session)
-    report = aw.load_file(ready_data_path, WARNINGS_FILE)
+    report = aw.load_file(ready_data_path, WARNINGS_FILES[0])
     assert "Warning" in str(report)
 
 
@@ -93,7 +93,7 @@ def test_loading_config(session, init_dest):
     assert analysis_config['name'] == analysis_sample
 
 
-def test_loading_config_with_error(session, init_dest):  
+def test_loading_config_with_error(session, init_dest):
     aw = init(session)
     analysis_config_path = aw.path_to_analysis_config(misconfigured_data_path, misconfigured_analysis_sample)
     with pytest.raises(RuntimeError) as excinfo:
@@ -108,8 +108,8 @@ def test_vcf_path(session, init_dest):
 
     with pytest.raises(RuntimeError) as excinfo:
         aw.path_to_vcf_file(empty_data_path, analysis_sample)
-    
-    expected_error = VCF_FILE_MISSING.format(empty_data_path + '/' + analysis_sample + VCF_POSTFIX)  
+
+    expected_error = VCF_FILE_MISSING.format(empty_data_path + '/' + analysis_sample + VCF_POSTFIX)
     assert expected_error in str(excinfo)
 
 
@@ -134,14 +134,14 @@ def test_extract_from_config_with_error(session, init_dest):
 def test_import_analysis(session, test_database, init_dest):
     test_database.refresh()
     aw = AnalysisWatcher(session, watch_path, dest_path)
-    
+
     analysis_config_data = aw.extract_from_config(ready_data_path, analysis_sample)
     aw.import_analysis(analysis_config_data)
 
     session.commit()
 
     with pytest.raises(RuntimeError) as excinfo:
-         aw.import_analysis(analysis_config_data)
+        aw.import_analysis(analysis_config_data)
 
     assert 'Analysis TestAnalysis-001 is already imported.' in str(excinfo)
 
@@ -153,6 +153,7 @@ def test_import_analysis(session, test_database, init_dest):
     ).all()
 
     assert len(analysis_stored) == 1
+
 
 def test_check_and_import(session, test_database, init_dest):
     aw = init(session)
@@ -168,19 +169,18 @@ def test_check_and_import(session, test_database, init_dest):
         analysis_config_data.gp_name,
         analysis_config_data.gp_version
     )
-     
+
     analysis_stored = session.query(sm.Analysis).filter(
         sm.Analysis.name == analysis_config_data.analysis_name,
         sm.Analysis.genepanel == db_genepanel
     ).all()
-
 
     assert len(analysis_stored) == 1
 
     files = os.listdir(watch_path)
     assert len(files) == 1
     assert files == [analysis_sample2]
-    
+
     # We do this only once, at the end of the tests. It is a bit fragile to use network to reset files and folders
     os.system('git checkout src/vardb/watcher/testdata/analyses/')
 
