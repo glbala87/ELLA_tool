9255305e7f07a6c18736647c51d57c0688039f7b
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-02-28 15:03:34 +0100

    [web] Update styling of verification selection on info-page

--- src/webui/src/js/widgets/analysisInfo.directive.js
+++ src/webui/src/js/widgets/analysisInfo.directive.js
@@ -16,4 +16,16 @@ import {AlleleStateHelper} from '../model/allelestatehelper';
 @Inject()
 export class AnalysisInfoController {
     constructor() {}
+
+    getAlleles() {
+        let sort = firstBy(a => a.allele.annotation.filtered[0].symbol)
+                   .thenBy(a => {
+                       let s = a.allele.annotation.filtered[0].HGVSc_short || a.allele.getHGVSgShort();
+                       let d = parseInt(s.match(/[cg]\.(\d+)/)[1]);
+                       return d
+                   });
+
+        let alleles = this.alleles.unclassified.concat(this.alleles.classified)
+        return alleles.sort(sort)
+    }
 }
