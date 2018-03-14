c4cd630b625f9b45e1877cc99d1bec6387b9d158
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-03-14 15:21:04 +0100

    [webui] Add missing setDirty for report inclusion

--- src/webui/src/js/store/modules/views/workflows/alleleSidebar/signals/includeReportToggled.js
+++ src/webui/src/js/store/modules/views/workflows/alleleSidebar/signals/includeReportToggled.js
@@ -1,7 +1,9 @@
 import { toggle } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
+import setDirty from '../../interpretation/actions/setDirty'
 
 export default [
+    setDirty,
     toggle(
         state`views.workflows.interpretation.selected.state.allele.${props`alleleId`}.report.included`
     )
