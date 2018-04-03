174a486629f0cbce23b980d31e06aae0b4ef2bdd
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-04-03 20:38:27 +0200

    [webui] Fix bug for suggested classification

--- src/webui/src/js/store/modules/views/workflows/alleleSidebar/sequences/allelesChanged.js
+++ src/webui/src/js/store/modules/views/workflows/alleleSidebar/sequences/allelesChanged.js
@@ -3,5 +3,10 @@ import { set } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import prepareSelectedAllele from '../actions/prepareSelectedAllele'
 import sortSections from '../actions/sortSections'
+import updateSuggestedClassification from '../../interpretation/sequences/updateSuggestedClassification'
 
-export default [sortSections, prepareSelectedAllele]
+export default [
+    sortSections,
+    prepareSelectedAllele,
+    [set(props`alleleId`, state`views.workflows.selectedAllele`), updateSuggestedClassification]
+]
--- src/webui/src/js/store/modules/views/workflows/alleleSidebar/signals/selectedAlleleChanged.js
+++ src/webui/src/js/store/modules/views/workflows/alleleSidebar/signals/selectedAlleleChanged.js
@@ -1,4 +1,8 @@
 import { set } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
+import updateSuggestedClassification from '../../interpretation/sequences/updateSuggestedClassification'
 
-export default [set(state`views.workflows.selectedAllele`, props`alleleId`)]
+export default [
+    set(state`views.workflows.selectedAllele`, props`alleleId`),
+    updateSuggestedClassification
+]
