c1fa367de43dcc763e84a196cf32242371b60c36
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-05-02 12:53:20 +0200

    [webui] Fix link in showAnalysesForAllele modal

--- src/webui/src/js/store/modules/modals/actions/getAnalysesForAllele.js
+++ src/webui/src/js/store/modules/modals/actions/getAnalysesForAllele.js
@@ -1,7 +1,10 @@
+import processAnalyses from '../../../common/helpers/processAnalyses'
+
 export default function getAnalysesForAllele({ http, props, path }) {
     return http
         .get(`alleles/${props.alleleId}/analyses/`)
         .then((response) => {
+            processAnalyses(response.result)
             return path.success({ result: response.result })
         })
         .catch((response) => {
