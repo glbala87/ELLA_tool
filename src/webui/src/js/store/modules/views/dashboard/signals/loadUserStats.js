2699c93e555338af823bf63876fd8a3e4b639deb
Erik Severinsen <monoburn@gmail.com>
2018-05-04 00:50:20 +0200

    toastr message on user dashboard error

--- src/webui/src/js/store/modules/views/dashboard/signals/loadUserStats.js
+++ src/webui/src/js/store/modules/views/dashboard/signals/loadUserStats.js
@@ -1,14 +1,12 @@
 import { set } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import getUserStats from '../actions/getUserStats'
+import toastr from "../../../../common/factories/toastr";
 
 export default [
     getUserStats,
     {
-        success: [
-            set(state`views.dashboard.data.userStats`, props`result`),
-            set(state`views.dashboard.error`, false)
-        ],
-        error: [set(state`views.dashboard.error`, true)]
+        success: [set(state`views.dashboard.data.userStats`, props`result`)],
+        error: [toastr('error', 'Failed finding user\'s work statistics')]
     }
 ]
--- src/webui/src/js/store/modules/views/dashboard/signals/loadUsers.js
+++ src/webui/src/js/store/modules/views/dashboard/signals/loadUsers.js
@@ -1,13 +1,16 @@
 import { set, get } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import getAllUsers from '../../../../../store/common/actions/getAllUsers'
+import toastr from "../../../../common/factories/toastr";
 
-// Populate state with others the users belonging to the same group
+// Populate state with users belonging to the same group
 
 function userByGroup({props, state })
 {
+    const EMPTY_LIST = []
     return {
-        usersInGroup: props.result.filter( (u) =>
+        // exclude current user
+        usersInGroup: (!props.result) ? EMPTY_LIST : props.result.filter( (u) =>
             u.group.name === state.get('app.user.group.name')
             && u.id !== state.get('app.user.id'))
     }
@@ -16,8 +19,8 @@ function userByGroup({props, state })
 export default [
     getAllUsers,
     {
-        success: [set(state`views.dashboard.error`, false)],
-        error: [set(state`views.dashboard.error`, true)]
+        success: [],
+        error: [toastr('error', 'Cannot find other users')]
     },
     userByGroup,
     set(state`views.dashboard.data.usersInGroup`, props`usersInGroup`)
