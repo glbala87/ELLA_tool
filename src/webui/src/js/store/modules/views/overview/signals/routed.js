a34ea6a69968a6a6ee15d38efb0aa072f4f9264e
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-05-11 13:15:03 +0000

    [webui] Remove commited debug code

--- src/webui/src/js/store/modules/views/overview/signals/routed.js
+++ src/webui/src/js/store/modules/views/overview/signals/routed.js
@@ -25,14 +25,14 @@ export default sequence('routed', [
                 {},
                 UPDATE_IMPORT_STATUS_INTERVAL * 1000,
                 true
-            ), // FIXME: Debug only
-            /* interval(
+            ),
+            interval(
                 'start',
                 'views.overview.updateOverviewTriggered',
                 {},
                 UPDATE_OVERVIEW_INTERVAL * 1000,
                 false
-            ), */
+            ),
             loadOverview
         ],
         invalid: [redirectToSection]
