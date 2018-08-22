370791e547d9bf715c435b147b472bc7e551a1c5
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-08-22 21:12:05 +0200

    [webui] Hide quick classification when not in Classification view

--- src/webui/src/js/views/alleleSidebar.directive.js
+++ src/webui/src/js/views/alleleSidebar.directive.js
@@ -1,12 +1,21 @@
 import app from '../ng-decorators'
 import { connect } from '@cerebral/angularjs'
 import { state, signal } from 'cerebral/tags'
+import { Compute } from 'cerebral'
 import template from './alleleSidebar.ngtmpl.html'
+
+const showQuickClassificationBtn = Compute(
+    state`views.workflows.selectedComponent`,
+    (selectedComponent) => {
+        return selectedComponent === 'Classification'
+    }
+)
 
 app.component('alleleSidebar', {
     templateUrl: 'alleleSidebar.ngtmpl.html',
     controller: connect(
         {
+            showQuickClassificationBtn,
             expanded: state`views.workflows.alleleSidebar.expanded`,
             toggleExpanded: signal`views.workflows.alleleSidebar.toggleExpanded`
         },
