196a056418044793c84c6f35b8dc2ec7bda3a7d1
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-08-24 12:33:06 +0200

    [webui] Finish showing samples on analysis info page

--- src/api/schemas/samples.py
+++ src/api/schemas/samples.py
@@ -5,12 +5,16 @@ class SampleSchema(Schema):
     class Meta:
         title = "Sample"
         description = 'Represents one sample. There can be many samples per analysis.'
-        fields = ('id',
-                  'identifier',
-                  'sample_type',
-                  'deposit_date',
-                  'affected',
-                  'proband',
-                  'sex',
-                  'family_id'
-                  )
+        fields = (
+            'id',
+            'identifier',
+            'sample_type',
+            'deposit_date',
+            'affected',
+            'proband',
+            'family_id',
+            'father_id',
+            'mother_id',
+            'sibling_id',
+            'sex'
+        )
--- src/webui/src/js/widgets/analysisInfo.directive.js
+++ src/webui/src/js/widgets/analysisInfo.directive.js
@@ -2,17 +2,15 @@ import app from '../ng-decorators'
 import { connect } from '@cerebral/angularjs'
 import { state, signal } from 'cerebral/tags'
 import isReadOnly from '../store/modules/views/workflows/computed/isReadOnly'
-import hasAlleles from '../store/modules/views/workflows/computed/hasAlleles'
-import sortAlleles from '../store/modules/views/workflows/computed/sortAlleles'
 import template from './analysisInfo.ngtmpl.html'
+import getSamples from '../store/modules/views/workflows/computed/getSamples'
 
 app.component('analysisInfo', {
     templateUrl: 'analysisInfo.ngtmpl.html',
     controller: connect(
         {
+            samples: getSamples,
             analysis: state`views.workflows.data.analysis`,
-            alleles: sortAlleles(state`views.workflows.data.alleles`),
-            hasAlleles,
             readOnly: isReadOnly
         },
         'AnalysisInfo'
