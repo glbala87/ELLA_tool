a6240ef6b1da1c729b4f39ff446a6b7d7ee0340b
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-09-14 14:04:32 +0200

    [e2e] Fix tests

--- src/webui/tests/e2e/pageobjects/addExcludedModal.js
+++ src/webui/tests/e2e/pageobjects/addExcludedModal.js
@@ -1,11 +1,23 @@
 var Page = require('./page')
 
 class AddExcludedModal extends Page {
-    get includeAlleleBtn() {
-        return browser.element('.id-add-excluded-modal .id-include-allele')
+    includeAllele(num) {
+        browser
+            .element(
+                `.id-add-excluded-modal .id-excluded section.allele-sidebar-list .nav-row:nth-child(${num +
+                    1}) .tab .added`
+            )
+            .click()
+        browser.pause(1000) // Can be a bit slow on very large samples
     }
-    get excludeAlleleBtn() {
-        return browser.element('.id-add-excluded-modal .id-exclude-allele')
+    excludeAllele(num) {
+        browser
+            .element(
+                `.id-add-excluded-modal .id-included section.allele-sidebar-list .nav-row:nth-child(${num +
+                    1}) .tab .added`
+            )
+            .click()
+        browser.pause(1000)
     }
     get closeBtn() {
         return browser.element('.id-add-excluded-modal .id-close')
