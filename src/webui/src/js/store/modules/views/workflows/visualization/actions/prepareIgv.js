fb177109a3db45a1642756954e8987365b93da3a
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-09-20 14:35:15 +0200

    [webui] Minor adjustments

--- src/webui/src/js/store/modules/views/workflows/visualization/actions/prepareIgv.js
+++ src/webui/src/js/store/modules/views/workflows/visualization/actions/prepareIgv.js
@@ -1,46 +1,5 @@
 import thenBy from 'thenby'
 
-const getAvailableTracks = (analysis, alleles) => {
-    let tracks = []
-
-    tracks.push({
-        id: 'genepanel',
-        name: 'Genepanel',
-        type: 'annotation',
-        url: `/api/v1/igv/genepanel/${analysis.genepanel.name}/${analysis.genepanel.version}/`,
-        format: 'bed',
-        indexed: false,
-        displayMode: 'EXPANDED',
-        order: 10,
-        show: false,
-        height: 60
-    })
-
-    tracks.push({
-        id: 'variants',
-        name: 'Variants',
-        url: `/api/v1/igv/variants/${analysis.id}/?allele_ids=${Object.keys(alleles).join(',')}`,
-        format: 'vcf',
-        indexed: false,
-        order: 11,
-        show: true,
-        visibilityWindow: Number.MAX_VALUE
-    })
-
-    tracks.push({
-        id: 'classifications',
-        name: 'Classifications',
-        url: '/api/v1/igv/classifications/',
-        format: 'bed',
-        indexed: false,
-        order: 11,
-        show: true,
-        visibilityWindow: Number.MAX_VALUE
-    })
-
-    return tracks
-}
-
 export default async function prepareIgv({ state, http }) {
     const igvReferenceConfig = state.get('app.config.igv.reference')
     state.set('views.workflows.visualization.igv.reference', {
@@ -54,17 +13,55 @@ export default async function prepareIgv({ state, http }) {
 
     const result = await http.get(`igv/tracks/${analysis.id}`)
     const tracks = {
-        global: [],
+        global: [
+            {
+                id: 'genepanel',
+                selected: false,
+                config: {
+                    name: 'Genepanel',
+                    type: 'annotation',
+                    url: `/api/v1/igv/genepanel/${analysis.genepanel.name}/${
+                        analysis.genepanel.version
+                    }/`,
+                    format: 'bed',
+                    indexed: false,
+                    displayMode: 'EXPANDED',
+                    order: 10,
+                    height: 60
+                }
+            },
+            {
+                id: 'classifications',
+                selected: false,
+                config: {
+                    name: 'Classifications',
+                    url: '/api/v1/igv/classifications/',
+                    format: 'bed',
+                    indexed: false,
+                    order: 11,
+                    visibilityWindow: Number.MAX_VALUE
+                }
+            }
+        ],
         user: [],
-        analysis: []
+        analysis: [
+            {
+                id: 'variants',
+                selected: true,
+                config: {
+                    name: 'Variants',
+                    url: `/api/v1/igv/variants/${analysis.id}/?allele_ids=${Object.keys(
+                        alleles
+                    ).join(',')}`,
+                    format: 'vcf',
+                    indexed: false,
+                    order: 12,
+                    visibilityWindow: Number.MAX_VALUE
+                }
+            }
+        ]
     }
-    for (const t of getAvailableTracks(analysis, alleles)) {
-        tracks.global.push({
-            selected: 'show' in t ? t.show : true,
-            id: t.id,
-            config: t
-        })
-    }
+
     for (const [category, categoryTracks] of Object.entries(result.result)) {
         for (const track of categoryTracks) {
             tracks[category].push({
