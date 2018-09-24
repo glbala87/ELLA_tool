0d1cdb0f5c02bac9a96ebaf7acbe5ef73f83ade9
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-09-24 12:12:41 +0200

    [webui] Fix bug in addExcludedModal when no filtered alleles

--- src/webui/src/js/store/modules/modals/addExcludedAlleles/actions/getAlleleIdsByGene.js
+++ src/webui/src/js/store/modules/modals/addExcludedAlleles/actions/getAlleleIdsByGene.js
@@ -1,6 +1,12 @@
-export default function({ http, path, props }) {
+export default function getAlleleIdsByGene({ http, path, props }) {
+    const alleleIds = props.alleleIds
+
+    if (!alleleIds.length) {
+        return path.success({ result: [] })
+    }
+
     return http
-        .get(`alleles/by-gene/?allele_ids=${props.alleleIds.join(',')}`)
+        .get(`alleles/by-gene/?allele_ids=${alleleIds.join(',')}`)
         .then((response) => {
             return path.success({ result: response.result })
         })
