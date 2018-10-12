e4428c4af03740bc5a28fc1e90d23ee3c9067b30
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-10-12 10:49:59 +0200

    [tests] Add cli tests for user commands

---
+++ src/cli/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+from vardb.util.testdatabase import TestDatabase
+from vardb.util import DB
+from click.testing import CliRunner
+from cli.cli import cli
+
+@pytest.yield_fixture
+def session(request):
+    db = DB()
+    db.connect()
+    session = db.session()
+
+    yield session
+    # Close session on teardown
+    session.close()
+    db.disconnect()
+
+# Will be shared among all tests
+@pytest.yield_fixture(scope="session", autouse=True)
+def test_database(request):
+    """
+    The TestDatabase object is yielded in order for the user to
+    be able to call refresh() when he wants a fresh database.
+    """
+    test_db = TestDatabase()
+    test_db.refresh()
+    yield test_db
+
+    # Cleanup database on teardown
+    test_db.cleanup()
+
+@pytest.fixture
+def run_command():
+    runner = CliRunner()
+    return lambda *args, **kwargs: runner.invoke(cli, *args, **kwargs)
+
