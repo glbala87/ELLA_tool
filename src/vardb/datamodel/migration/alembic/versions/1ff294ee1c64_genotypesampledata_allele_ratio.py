212423c4aea4a6b37adb5fc99cdf8087f6805b2d
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-10-22 20:24:24 +0200

    [datamodel] Add migration

---
+++ src/vardb/datamodel/migration/alembic/versions/1ff294ee1c64_genotypesampledata_allele_ratio.py
@@ -0,0 +1,27 @@
+"""genotypesampledata allele_ratio
+
+Revision ID: 1ff294ee1c64
+Revises: d099628590c9
+Create Date: 2018-10-22 17:56:30.684035
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1ff294ee1c64'
+down_revision = 'd099628590c9'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql import column, table
+from sqlalchemy.dialects import postgresql
+
+
+def upgrade():
+    op.add_column(u'genotypesampledata', sa.Column('allele_ratio', sa.Float(), nullable=True))
+    # Migrating the existing data isn't necessary and would be error prone
+
+
+def downgrade():
+    raise NotImplementedError('Downgrade not possible')
