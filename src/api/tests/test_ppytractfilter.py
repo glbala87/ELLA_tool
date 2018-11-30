1b0c9c37e63f48a07764ddd96f403e3983e62b63
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-11-30 12:50:46 +0100

    [tests] Fix ppy filter tests. Cleanup test code. Improve hypothesis sample generation.

--- src/api/tests/test_ppytractfilter.py
+++ src/api/tests/test_ppytractfilter.py
@@ -2,11 +2,10 @@
 Integration/unit test for the AlleleFilter module.
 Since it consists mostly of database queries, it's tested on a live database.
 """
-import copy
 import pytest
 
 from api.allelefilter.polypyrimidinetractfilter import PolypyrimidineTractFilter
-from vardb.datamodel import allele, annotation, gene, annotationshadow, assessment
+from vardb.datamodel import allele, annotation, gene, assessment
 
 import hypothesis as ht
 import hypothesis.strategies as st
@@ -14,20 +13,23 @@ import hypothesis.strategies as st
 
 GLOBAL_CONFIG = {}
 
-
-def check_valid_allele(vcf_ref, vcf_alt):
-    ht.assume(vcf_ref != vcf_alt)
-
-
 @st.composite
 def allele_positions(draw, chromosome, start, end):
     start_position = draw(st.integers(min_value=start, max_value=end))
-    # end_position = draw(st.integers(min_value=start_position+1, max_value=start_position+50))
-    vcf_ref = draw(st.text(alphabet=["A", "C", "G","T"], min_size=1, max_size=2))
+    vcf_ref = draw(st.text(alphabet=["A", "C", "G","T"], min_size=1, max_size=3))
     vcf_alt = draw(st.text(alphabet=["A", "C", "G","T"], min_size=1, max_size=2))
     ht.assume(vcf_alt != vcf_ref)
     ht.assume(vcf_ref[0] != vcf_alt[0] or len(vcf_alt) != len(vcf_ref))
     ht.assume(not (vcf_ref[0] == vcf_alt[0] and len(vcf_alt) > 1 and len(vcf_ref) > 1))
+
+    # Skew tests away from ins and indels (len(vcf_alt) > 1)
+    r = draw(st.randoms())
+    if r.random() < 0.8:
+        ht.assume(len(vcf_alt)==1)
+
+    # Skew tests towards SNPs
+    if r.random() < 0.5:
+        ht.assume(len(vcf_alt)==1 and len(vcf_ref)==1)
 
     return (chromosome, start_position, vcf_ref, vcf_alt)
 
@@ -41,10 +43,6 @@ def create_allele(data=None):
     default_allele_data = {
             "chromosome": "1",
             "start_position": allele_start,
-            # "open_end_position": allele_start+1,
-            # "change_from": "A",
-            # "change_to": "T",
-            # "change_type": "SNP",
             "vcf_pos": allele_start+1,
             "vcf_ref": "A",
             "vcf_alt": "T"
@@ -99,38 +97,15 @@ def create_allele(data=None):
     )
 
 
-def create_annotation(annotations, allele=None):
-    return annotation.Annotation(
-        annotations=annotations,
-        allele=allele
-    )
-
-
-def create_allele_with_annotation(session, annotations=None, allele_data=None):
-    al = create_allele(data=allele_data)
-    session.add(al)
-    if annotations is not None:
-        an = create_annotation(annotations, allele=al)
-        session.add(an)
-    else:
-        an = None
-
-    return al, an
-
-
 def create_genepanel(genepanel_config):
     # Create fake genepanel for testing purposes
 
-    g1_ad = gene.Gene(hgnc_id=int(1e6), hgnc_symbol="GENE1AD")
-    g1_ar = gene.Gene(hgnc_id=int(2e6), hgnc_symbol="GENE1AR")
-    g2 = gene.Gene(hgnc_id=int(3e6), hgnc_symbol="GENE2")
-    g3 = gene.Gene(hgnc_id=int(4e6), hgnc_symbol="GENE3")
-    g4 = gene.Gene(hgnc_id=int(5e6), hgnc_symbol="GENE4")
-    g5 = gene.Gene(hgnc_id=int(6e6), hgnc_symbol="GENE5")
+    g1 = gene.Gene(hgnc_id=int(1e6), hgnc_symbol="GENE1")
+    g2 = gene.Gene(hgnc_id=int(2e6), hgnc_symbol="GENE2")
 
-    t1_ad = gene.Transcript(
-        gene=g1_ad,
-        transcript_name='NM_1AD.1',
+    t1_forward = gene.Transcript(
+        gene=g1,
+        transcript_name='NM_1.1',
         type='RefSeq',
         genome_reference='',
         chromosome='1',
@@ -143,22 +118,7 @@ def create_genepanel(genepanel_config):
         exon_ends=[1160, 1260, 1360, 1460]
     )
 
-    t1_ar = gene.Transcript(
-        gene=g1_ar,
-        transcript_name='NM_1AR.1',
-        type='RefSeq',
-        genome_reference='',
-        chromosome='1',
-        tx_start=1000,
-        tx_end=1500,
-        strand='+',
-        cds_start=1230,
-        cds_end=1430,
-        exon_starts=[1100, 1200, 1300, 1400],
-        exon_ends=[1160, 1260, 1360, 1460]
-    )
-
-    t2 = gene.Transcript(
+    t2_reverse = gene.Transcript(
         gene=g2,
         transcript_name='NM_2.1',
         type='RefSeq',
@@ -166,68 +126,11 @@ def create_genepanel(genepanel_config):
         chromosome='2',
         tx_start=2000,
         tx_end=2500,
-        strand='+',
+        strand='-',
         cds_start=2230,
         cds_end=2430,
         exon_starts=[2100, 2200, 2300, 2400],
         exon_ends=[2160, 2260, 2360, 2460]
-    )
-
-    t3 = gene.Transcript(
-        gene=g3,
-        transcript_name='NM_3.1',
-        type='RefSeq',
-        genome_reference='',
-        chromosome='3',
-        tx_start=3000,
-        tx_end=3500,
-        strand='+',
-        cds_start=3230,
-        cds_end=3430,
-        exon_starts=[3100, 3200, 3300, 3400],
-        exon_ends=[3160, 3260, 3360, 3460]
-    )
-
-    t4 = gene.Transcript(
-        gene=g4,
-        transcript_name='NM_4.1',
-        type='RefSeq',
-        genome_reference='',
-        chromosome='4',
-        tx_start=4000,
-        tx_end=4500,
-        strand='+',
-        cds_start=4230,
-        cds_end=4430,
-        exon_starts=[4100, 4200, 4300, 4400],
-        exon_ends=[4160, 4260, 4360, 4460]
-    )
-
-    t5_reverse = gene.Transcript(
-        gene=g5,
-        transcript_name='NM_5.1',
-        type='RefSeq',
-        genome_reference='',
-        chromosome='5',
-        tx_start=5000,
-        tx_end=5500,
-        strand='-',
-        cds_start=5230,
-        cds_end=5430,
-        exon_starts=[5100, 5200, 5300, 5400],
-        exon_ends=[5160, 5260, 5360, 5460]
-    )
-
-    p1 = gene.Phenotype(
-        gene=g1_ad,
-        inheritance='AD',
-        description='P1'
-    )
-
-    p2 = gene.Phenotype(
-        gene=g1_ar,
-        inheritance='AD,AR',
-        description='P2'
     )
 
     genepanel = gene.Genepanel(
@@ -236,8 +139,8 @@ def create_genepanel(genepanel_config):
         genome_reference='GRCh37'
     )
 
-    genepanel.transcripts = [t1_ad, t1_ar, t2, t3, t4, t5_reverse]
-    genepanel.phenotypes = [p1, p2]
+    genepanel.transcripts = [t1_forward, t2_reverse]
+    genepanel.phenotypes = []
     return genepanel
 
 
@@ -257,21 +160,44 @@ class TestPolypyrimidineTractFilter(object):
     @ht.example(('1', 1280, "AC", "A"), False)
     @ht.example(('1', 1280, "CT", "C"), True)
     @ht.example(('1', 1397, 'C', 'T'), True)
+    @ht.example(('2', 2363, 'AG', 'A'), True)
+    @ht.example(('2', 2363, 'A', 'G'), True)
+    @ht.example(('2', 2363, 'G', 'A'), True)
+    @ht.example(('2', 2297, 'C', 'T'), False)
+    @ht.example(('2', 2370, 'C', 'T'), False)
+    @ht.example(('2', 2370, "AAG", 'A'), True)
     @ht.given(
         st.one_of(
+            # Fill up with positions that are close to ppy regions, to get
+            # a better sampling
+            allele_positions('1', 1075, 1105),  # t1, positive strand
+            allele_positions('1', 1175, 1205),  # t1, positive strand
+            allele_positions('1', 1275, 1305),  # t1, positive strand
+            allele_positions('1', 1375, 1405),  # t1, positive strand
+            allele_positions('2', 2155, 2185),  # t2, negative strand
+            allele_positions('2', 2255, 2285),  # t2, negative strand
+            allele_positions('2', 2355, 2385),  # t2, negative strand
+            allele_positions('2', 2455, 2485),  # t2, negative strand
+            allele_positions('1', 1085, 1095),  # t1, positive strand
+            allele_positions('1', 1185, 1195),  # t1, positive strand
+            allele_positions('1', 1285, 1295),  # t1, positive strand
+            allele_positions('1', 1375, 1405),  # t1, positive strand
+            allele_positions('2', 2155, 2185),  # t2, negative strand
+            allele_positions('2', 2255, 2285),  # t2, negative strand
+            allele_positions('2', 2355, 2385),  # t2, negative strand
+            allele_positions('2', 2455, 2485),  # t2, negative strand
             allele_positions('1', 900, 1600),  # t1, positive strand
-            allele_positions('5', 4900, 5600)),  # t5, negative strand
+            allele_positions('2', 1900, 2600),  # t2, negative strand
+        ),
         st.just(None)
     )
     @ht.settings(deadline=1500)
-    def test_genomic_region_filtering(self, session, positions, manually_curated_result):
+    def test_ppy_filtering(self, session, positions, manually_curated_result):
         """
         Tests both using manually curated test and parallell implementation in Python.
         """
-
         chromosome, start_position, vcf_ref, vcf_alt = positions
-        al, _ = create_allele_with_annotation(session,
-            None,
+        al = create_allele(
             {
                 "chromosome": chromosome,
                 "start_position": start_position,
@@ -279,6 +205,7 @@ class TestPolypyrimidineTractFilter(object):
                 "vcf_alt": vcf_alt
             }
         )
+        session.add(al)
 
         session.flush()
 
@@ -299,10 +226,13 @@ class TestPolypyrimidineTractFilter(object):
             else:
                 assert result[gp_key] == set([])
 
+
         if al.change_type not in ["del", "SNP"]:
             assert result[gp_key] == set()
-        elif len(al.change_from) > 2 or len(al.change_to) > 1:
+            return
+        elif al.change_type == 'del' and len(al.change_from) > 2:
             assert result[gp_key] == set()
+            return
 
         genepanel = session.query(gene.Genepanel).filter(
             gene.Genepanel.name == 'testpanel',
@@ -345,8 +275,7 @@ class TestPolypyrimidineTractFilter(object):
                 assert result[gp_key] == set([al.id])
             elif strand == '+' and al.change_type == 'del' and set(al.change_from) - set('CT') == set() and al.vcf_alt != 'A' and len(al.change_from) <= 2:
                 assert result[gp_key] == set([al.id])
-            elif strand == '-' and al.change_type == 'del' and set(al.change_from) - set('GA') == set() and al.vcf_alt != 'G' and len(al.change_from) <= 2:
+            elif strand == '-' and al.change_type == 'del' and set(al.change_from) - set('GA') == set() and al.vcf_alt != 'C' and len(al.change_from) <= 2:
                 assert result[gp_key] == set([al.id])
             else:
                 assert result[gp_key] == set()
-
