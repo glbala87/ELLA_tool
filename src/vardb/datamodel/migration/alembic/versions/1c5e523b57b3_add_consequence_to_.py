616bac95c791a7b57e1959df392e13f56eb8a468
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-12-07 11:05:07 +0100

    [migration] Fix migration head

--- src/vardb/datamodel/migration/alembic/versions/1c5e523b57b3_add_consequence_to_.py
+++ src/vardb/datamodel/migration/alembic/versions/1c5e523b57b3_add_consequence_to_.py
@@ -1,14 +1,14 @@
 """Add consequence to interpretationsnapshot
 
 Revision ID: 1c5e523b57b3
-Revises: 250c9c10baee
+Revises: 61b990b84ed2
 Create Date: 2018-11-06 11:59:03.515917
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '1c5e523b57b3'
-down_revision = '250c9c10baee'
+down_revision = '61b990b84ed2'
 branch_labels = None
 depends_on = None
 
