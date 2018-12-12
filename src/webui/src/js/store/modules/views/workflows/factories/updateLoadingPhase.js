15a105bfac1f247f69b0f78fec88d64e1cd67a33
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2018-12-12 12:59:56 +0100

    [webui] Remove ... from loading text

--- src/webui/src/js/store/modules/views/workflows/factories/updateLoadingPhase.js
+++ src/webui/src/js/store/modules/views/workflows/factories/updateLoadingPhase.js
@@ -16,13 +16,13 @@ export default function updateLoadingPhase(phase) {
             loadingText = ''
         } else if (phase === 'filtering') {
             if (showLoadingText) {
-                loadingText = `Filtering ${numTotalAlleles} variants...`
+                loadingText = `Filtering ${numTotalAlleles} variants`
             }
         } else if (phase === 'variants') {
             if (showLoadingText) {
                 const numVariants = state.get('views.workflows.interpretation.selected.allele_ids')
                     .length
-                loadingText = `Loading ${numVariants} variants...`
+                loadingText = `Loading ${numVariants} variants`
             }
         } else if (phase === 'done') {
             loaded = true
