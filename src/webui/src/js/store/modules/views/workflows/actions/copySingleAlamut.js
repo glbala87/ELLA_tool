dc80ec6b9820a49986b7d6d7528da11cd1face4a
Øyvind Evju <oyvind.evju@medisin.uio.no>
2018-12-18 13:53:59 +0100

    [web] Remove unused files (webpack deadcode detection)

--- src/webui/src/js/store/modules/views/workflows/actions/copySingleAlamut.js
+++
@@ -1,7 +0,0 @@
-import { state } from 'cerebral/tags'
-import sortAlleles from '../computed/sortAlleles'
-
-export default function copyAllAlamut({ clipboard, resolve }) {
-    let alleles = resolve.value(sortAlleles(state`views.workflows.data.alleles`, false))
-    clipboard.copy(alleles.map((a) => a.formatted.alamut).join('\n'))
-}
