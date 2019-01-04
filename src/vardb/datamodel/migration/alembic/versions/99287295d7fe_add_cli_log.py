ca3bf61207ec2994a40f113e79ee94ad897ffa63
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2019-01-04 10:50:20 +0100

    [migration] Add migration for cli log

---
+++ src/vardb/datamodel/migration/alembic/versions/99287295d7fe_add_cli_log.py
@@ -0,0 +1,34 @@
+"""Add cli log
+
+Revision ID: 99287295d7fe
+Revises: ec5fb8b62e4a
+Create Date: 2019-01-04 09:34:32.410610
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "99287295d7fe"
+down_revision = "2a1becd831ab"
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        "clilog",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("time", sa.DateTime(timezone=True), nullable=False),
+        sa.Column("user", sa.String(), nullable=False),
+        sa.Column("group", sa.String(), nullable=False),
+        sa.Column("groupcommand", sa.String(), nullable=False),
+        sa.Column("command", sa.String(), nullable=False),
+        sa.Column("output", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_clilog")),
+    )
+
+
+def downgrade():
+    op.drop_table("clilog")
