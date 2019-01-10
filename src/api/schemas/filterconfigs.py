0060bfe4124cafd5b8ee0373be5c47feb9ad71f3
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-01-10 15:27:00 +0100

    [api] Fix files not autoformatted by black

--- src/api/schemas/filterconfigs.py
+++ src/api/schemas/filterconfigs.py
@@ -1,8 +1,6 @@
 from marshmallow import Schema, fields
 
+
 class FilterConfigSchema(Schema):
     class Meta:
-        fields = ('id',
-                  'name',
-                  'filterconfig',
-        )
+        fields = ("id", "name", "filterconfig")
