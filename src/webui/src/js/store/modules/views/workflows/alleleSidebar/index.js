a0dd7278a07384843fb3f9bcedacd1c7b9a99cce
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-01-14 10:56:11 +0100

    [web] Add dropdown for selecting filter config

--- src/webui/src/js/store/modules/views/workflows/alleleSidebar/index.js
+++ src/webui/src/js/store/modules/views/workflows/alleleSidebar/index.js
@@ -4,6 +4,7 @@ import reviewedClicked from './signals/reviewedClicked'
 import orderByChanged from './signals/orderByChanged'
 import toggleExpanded from './signals/toggleExpanded'
 import quickClassificationClicked from './signals/quickClassificationClicked'
+import filterconfigChanged from './signals/filterconfigChanged'
 
 export default Module({
     state: {},
@@ -12,6 +13,7 @@ export default Module({
         reviewedClicked,
         orderByChanged,
         toggleExpanded,
-        quickClassificationClicked
+        quickClassificationClicked,
+        filterconfigChanged
     }
 })
