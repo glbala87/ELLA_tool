cfcc9519ffc9976c6d493af3ce1765ecbe8ec91f
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-01-18 14:41:06 +0100

    [web] Set default filter config in loadInterpretationData

--- src/webui/src/js/store/modules/views/workflows/actions/setDefaultFilterConfig.js
+++ src/webui/src/js/store/modules/views/workflows/actions/setDefaultFilterConfig.js
@@ -1,9 +1,18 @@
-export default function({ state }) {
+import getSelectedInterpretation from '../computed/getSelectedInterpretation'
+
+export default function({ state, resolve }) {
     let stateFilterConfigId = state.get('views.workflows.interpretation.state.filterconfigId')
     let availableFilterConfigIds = state
         .get('views.workflows.data.filterconfigs')
         .map((fc) => fc.id)
-    if (!availableFilterConfigIds.includes(stateFilterConfigId)) {
+
+    const selectedInterpretation = resolve.value(getSelectedInterpretation)
+    let current = state.get('views.workflows.interpretation.selectedId') === 'current'
+    let isDone = selectedInterpretation.status === 'Done'
+
+    // Only set default filter config when showing current data or in an ongoing interpretation,
+    // and the selected filter config is not available for filtering
+    if ((current || !isDone) && !availableFilterConfigIds.includes(stateFilterConfigId)) {
         let defaultFilterConfigId = state.get('views.workflows.data.filterconfigs')[0].id
         state.set('views.workflows.interpretation.state.filterconfigId', defaultFilterConfigId)
     }
--- src/webui/src/js/store/modules/views/workflows/signals/loadInterpretationData.js
+++ src/webui/src/js/store/modules/views/workflows/signals/loadInterpretationData.js
@@ -1,4 +1,4 @@
-import { set } from 'cerebral/operators'
+import { set, when } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import { sequence, parallel } from 'cerebral'
 import progress from '../../../../common/factories/progress'
@@ -11,9 +11,15 @@ import getFilteredAlleles from '../actions/getFilteredAlleles'
 import toast from '../../../../common/factories/toast'
 import updateLoadingPhase from '../factories/updateLoadingPhase'
 import getFilterConfig from '../../../modals/addExcludedAlleles/actions/getFilterConfig'
+import setDefaultFilterConfig from '../actions/setDefaultFilterConfig'
 
 export default sequence('loadInterpretationData', [
     progress('start'),
+    when(state`views.workflows.type`, (type) => type === 'analysis'),
+    {
+        true: [setDefaultFilterConfig],
+        false: []
+    },
     getFilterConfig,
     {
         success: [set(state`views.workflows.interpretation.data.filterConfig`, props`result`)],
