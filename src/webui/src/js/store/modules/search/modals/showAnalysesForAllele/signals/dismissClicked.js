6b35655a6c1ec1e672c6bc5e2f81c202edefa556
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2019-03-26 12:47:01 +0100

    [webui] Fix regression in showAnalysesForAllele modal

--- src/webui/src/js/store/modules/search/modals/showAnalysesForAllele/signals/dismissClicked.js
+++ src/webui/src/js/store/modules/search/modals/showAnalysesForAllele/signals/dismissClicked.js
@@ -1,4 +1,4 @@
 import { set } from 'cerebral/operators'
 import { state } from 'cerebral/tags'
 
-export default [set(state`search.modals.showAnalysesForAllele.show`, false)]
+export default [set(state`search.modals.showAnalysesForAllele`, { show: false })]
