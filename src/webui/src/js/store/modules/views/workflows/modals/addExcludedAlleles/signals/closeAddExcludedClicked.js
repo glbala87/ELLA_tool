19c7d5c6c77f84086b8be63d0b804b657e7467d6
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2019-05-09 10:20:46 +0200

    [webui] Fix loading of genepanel when including filtered alleles

--- src/webui/src/js/store/modules/views/workflows/modals/addExcludedAlleles/signals/closeAddExcludedClicked.js
+++ src/webui/src/js/store/modules/views/workflows/modals/addExcludedAlleles/signals/closeAddExcludedClicked.js
@@ -3,10 +3,12 @@ import { state, props } from 'cerebral/tags'
 import setManuallyAddedAlleleIds from '../actions/setManuallyAddedAlleleIds'
 import loadAlleles from '../../../sequences/loadAlleles'
 import loadVisualization from '../../../visualization/sequences/loadVisualization'
+import loadGenepanel from '../../../sequences/loadGenepanel'
 
 export default [
     setManuallyAddedAlleleIds,
     set(state`views.workflows.modals.addExcludedAlleles.show`, false),
+    loadGenepanel,
     loadAlleles,
     loadVisualization
 ]
