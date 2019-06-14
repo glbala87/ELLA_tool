8b67f92051d45bd20caac76763f60151210f1f7c
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2019-06-14 13:34:58 +0200

    [webui] toggle classification on un-re-evaluate

--- src/webui/src/js/store/modules/views/workflows/interpretation/signals/reuseAlleleAssessmentClicked.js
+++ src/webui/src/js/store/modules/views/workflows/interpretation/signals/reuseAlleleAssessmentClicked.js
@@ -1,9 +1,10 @@
-import { set } from 'cerebral/operators'
+import { set, when } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import toggleReuseAlleleAssessment from '../actions/toggleReuseAlleleAssessment'
 import copyExistingAlleleAssessments from '../../actions/copyExistingAlleleAssessments'
 import autoReuseExistingReferenceAssessments from '../actions/autoReuseExistingReferenceAssessments'
 import isReadOnly from '../operators/isReadOnly'
+import allelesChanged from '../../alleleSidebar/sequences/allelesChanged';
 
 export default [
     isReadOnly,
@@ -11,15 +12,24 @@ export default [
         false: [
             toggleReuseAlleleAssessment,
             copyExistingAlleleAssessments,
-            set(
-                state`views.workflows.interpretation.state.allele.${props`alleleId`}.alleleassessment.classification`,
-                null
-            ),
+            // resets dropdown in UI, but causes problem with unit tests.
+            // `classification: null` prevents the re-filling of data and so can't be finalized
+            when(state`views.workflows.interpretation.state.allele.${props`alleleId`}.alleleassessment.reuse`),
+            {
+                true: [],
+                false: [
+                    set(
+                        state`views.workflows.interpretation.state.allele.${props`alleleId`}.alleleassessment.classification`,
+                        null
+                    )
+                ]
+            },
             set(
                 state`views.workflows.interpretation.state.allele.${props`alleleId`}.referenceassessments`,
                 []
             ),
-            autoReuseExistingReferenceAssessments
+            autoReuseExistingReferenceAssessments,
+            allelesChanged
         ],
         true: []
     }
