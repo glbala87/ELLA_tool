3b052c41999580de71fe38dad9a88c3db8528552
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2019-09-03 10:01:11 +0200

    [webui] Disable batch import textarea when processed

--- src/webui/src/js/store/modules/views/overview/import/sequences/updateCandidatesFilter.js
+++ src/webui/src/js/store/modules/views/overview/import/sequences/updateCandidatesFilter.js
@@ -9,10 +9,6 @@ export default [
     equals(state`views.overview.import.custom.selectedFilterMode`),
     {
         single: [
-            //set(state`views.overview.import.custom.candidates.filter`, props`filter`),
-            //set(state`views.overview.import.custom.candidates.filterBatch`, ''),
-            //set(state`views.overview.import.custom.candidates.filterBatchProcessed`, false),
-            // If using single mode, reset batch mode to avoid inconsistent UI
             filterAndFlattenGenepanel(
                 'views.overview.import.data.genepanel',
                 'views.overview.import.custom.candidates.filteredFlattened',
