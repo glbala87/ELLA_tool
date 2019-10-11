99a5d2c01987034ccf779389a7ed0437703bb6b7
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-10-11 16:06:53 +0200

    [filter] Add allele ratio to quality filter

--- src/api/allelefilter/qualityfilter.py
+++ src/api/allelefilter/qualityfilter.py
@@ -1,4 +1,4 @@
-from sqlalchemy import or_
+from sqlalchemy import or_, and_
 from vardb.datamodel import sample, genotype
 
 
@@ -11,6 +11,17 @@ class QualityFilter(object):
         """
         Returns allele_ids that can be filtered _out_ from an analysis.
         """
+        assert filter_config.get("qual") or filter_config.get("allele_ratio")
+
+        quality_filters = []
+        if "qual" in filter_config:
+            quality_filters.append(genotype.Genotype.variant_quality < filter_config["qual"])
+
+        if "allele_ratio" in filter_config:
+            quality_filters.append(
+                genotype.GenotypeSampleData.allele_ratio < filter_config["allele_ratio"]
+            )
+        assert len(quality_filters)
 
         result = dict()
         for analysis_id, allele_ids in analysis_allele_ids.items():
@@ -20,14 +31,16 @@ class QualityFilter(object):
 
             quality_filtered = (
                 self.session.query(genotype.Genotype.allele_id, genotype.Genotype.secondallele_id)
+                .join(genotype.GenotypeSampleData)
                 .join(sample.Sample)
                 .filter(
                     or_(
                         genotype.Genotype.allele_id.in_(allele_ids),
                         genotype.Genotype.secondallele_id.in_(allele_ids),
                     ),
-                    genotype.Genotype.variant_quality < filter_config["qual"],
+                    and_(*quality_filters),
                     sample.Sample.analysis_id == analysis_id,
+                    sample.Sample.proband.is_(True),
                 )
                 .all()
             )
@@ -35,6 +48,7 @@ class QualityFilter(object):
             quality_filtered = [a[0] for a in quality_filtered] + [
                 a[1] for a in quality_filtered if a[1]
             ]
+
             result[analysis_id] = set(quality_filtered)
 
         return result
