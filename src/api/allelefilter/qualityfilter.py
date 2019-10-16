f86d584abfed163640e3237c2e1bfc2bc18aaa03
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-10-16 14:30:23 +0200

    [api] Use get_genotype_temp_table in quality filter

--- src/api/allelefilter/qualityfilter.py
+++ src/api/allelefilter/qualityfilter.py
@@ -1,5 +1,5 @@
-from sqlalchemy import or_, and_
-from vardb.datamodel import sample, genotype
+from vardb.datamodel import sample
+from api.allelefilter.genotypetable import get_genotype_temp_table
 
 
 class QualityFilter(object):
@@ -11,17 +11,7 @@ class QualityFilter(object):
         """
         Returns allele_ids that can be filtered _out_ from an analysis.
         """
-        assert filter_config.get("qual") or filter_config.get("allele_ratio")
-
-        quality_filters = []
-        if "qual" in filter_config:
-            quality_filters.append(genotype.Genotype.variant_quality < filter_config["qual"])
-
-        if "allele_ratio" in filter_config:
-            quality_filters.append(
-                genotype.GenotypeSampleData.allele_ratio < filter_config["allele_ratio"]
-            )
-        assert len(quality_filters)
+        assert "qual" in filter_config or "allele_ratio" in filter_config
 
         result = dict()
         for analysis_id, allele_ids in analysis_allele_ids.items():
@@ -29,25 +19,39 @@ class QualityFilter(object):
                 result[analysis_id] = set()
                 continue
 
-            quality_filtered = (
-                self.session.query(genotype.Genotype.allele_id, genotype.Genotype.secondallele_id)
-                .join(genotype.GenotypeSampleData)
-                .join(sample.Sample)
-                .filter(
-                    or_(
-                        genotype.Genotype.allele_id.in_(allele_ids),
-                        genotype.Genotype.secondallele_id.in_(allele_ids),
-                    ),
-                    and_(*quality_filters),
-                    sample.Sample.analysis_id == analysis_id,
-                    sample.Sample.proband.is_(True),
-                )
-                .all()
+            sample_id, sample_identifier = (
+                self.session.query(sample.Sample.id, sample.Sample.identifier)
+                .filter(sample.Sample.analysis_id == analysis_id, sample.Sample.proband.is_(True))
+                .one()
+            )
+            genotype_table = get_genotype_temp_table(
+                self.session,
+                allele_ids,
+                [sample_id],
+                genotype_extras={"qual": "variant_quality"},
+                genotypesampledata_extras={"ar": "allele_ratio"},
             )
 
-            quality_filtered = [a[0] for a in quality_filtered] + [
-                a[1] for a in quality_filtered if a[1]
-            ]
+            quality_filters = []
+            if "qual" in filter_config:
+                quality_filters.append(
+                    getattr(genotype_table.c, sample_identifier + "_qual") < filter_config["qual"]
+                )
+
+            if "allele_ratio" in filter_config:
+                quality_filters.extend(
+                    [
+                        getattr(genotype_table.c, sample_identifier + "_ar")
+                        < filter_config["allele_ratio"],
+                        getattr(genotype_table.c, sample_identifier + "_ar")
+                        != 0.0,  # Allele ratios can sometimes be misleading 0.0. Avoid filtering these out.
+                    ]
+                )
+            assert len(quality_filters)
+
+            quality_filtered = (
+                self.session.query(genotype_table.c.allele_id).filter(*quality_filters).scalar_all()
+            )
 
             result[analysis_id] = set(quality_filtered)
 
