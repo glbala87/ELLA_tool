8f4a2e085bc8398ef5f4e58c68991b8af2bf368d
Øyvind Evju <oyvind.evju@medisin.uio.no>
2019-10-16 16:03:26 +0200

    [test] Improve readability of quality filter test

--- src/api/tests/test_qualityfilter.py
+++ src/api/tests/test_qualityfilter.py
@@ -53,14 +53,16 @@ def add_data(session, gt_data):
         session.add(al)
         session.flush()
 
-        gt = genotype.Genotype(allele_id=al.id, sample_id=sa.id, variant_quality=data["qual"])
+        gt = genotype.Genotype(
+            allele_id=al.id, sample_id=sa.id, variant_quality=data["variant_quality"]
+        )
         session.add(gt)
         session.flush()
 
         gsd = genotype.GenotypeSampleData(
             sample_id=sa.id,
             genotype_id=gt.id,
-            allele_ratio=data["ar"],
+            allele_ratio=data["genotype_allele_ratio"],
             type="Heterozygous",
             secondallele=False,
             multiallelic=False,
@@ -77,8 +79,8 @@ def add_data(session, gt_data):
 @st.composite
 def filter_data(draw):
     return {
-        "qual": draw(st.integers(min_value=0, max_value=250)),
-        "ar": draw(st.floats(min_value=0, max_value=1.0, width=16)),
+        "variant_quality": draw(st.integers(min_value=0, max_value=250)),
+        "genotype_allele_ratio": draw(st.floats(min_value=0, max_value=1.0, width=16)),
     }
 
 
@@ -96,21 +98,41 @@ def filter_config(draw):
     return fc
 
 
-@ht.example([{"ar": 0.24, "qual": 101}], {"qual": 100, "allele_ratio": 0.25}, False)
-@ht.example([{"ar": 0.26, "qual": 99}], {"qual": 100, "allele_ratio": 0.25}, False)
-@ht.example([{"ar": 0.24, "qual": 99}], {"qual": 100, "allele_ratio": 0.25}, True)
-@ht.example([{"ar": 0.24, "qual": 99}], {"allele_ratio": 0.25}, True)
-@ht.example([{"ar": 0.26, "qual": 99}], {"allele_ratio": 0.25}, False)
-@ht.example([{"ar": 0.24, "qual": 99}], {"qual": 100}, True)
-@ht.example([{"ar": 0.24, "qual": 101}], {"qual": 100}, False)
 @ht.example(
-    [{"ar": 0.24, "qual": None}], {"qual": 100}, False
+    [{"genotype_allele_ratio": 0.24, "variant_quality": 101}],
+    {"qual": 100, "allele_ratio": 0.25},
+    False,
+)  # Should not filter where only one criteria if fulfilled (in this case, allele_ratio)
+@ht.example(
+    [{"genotype_allele_ratio": 0.26, "variant_quality": 99}],
+    {"qual": 100, "allele_ratio": 0.25},
+    False,
+)  # Should not filter where only one criteria if fulfilled (in this case, variant_quality)
+@ht.example(
+    [{"genotype_allele_ratio": 0.24, "variant_quality": 99}],
+    {"qual": 100, "allele_ratio": 0.25},
+    True,
+)  # Should filter when both criteria fulfilled
+@ht.example(
+    [{"genotype_allele_ratio": 0.24, "variant_quality": 99}], {"allele_ratio": 0.25}, True
+)  # Should disregard variant quality when not set in filter config
+@ht.example(
+    [{"genotype_allele_ratio": 0.26, "variant_quality": 99}], {"allele_ratio": 0.25}, False
+)  # Should disregard variant quality when not set in filter config
+@ht.example(
+    [{"genotype_allele_ratio": 0.24, "variant_quality": 99}], {"qual": 100}, True
+)  # Should disregard allele ratio when not set in filter config
+@ht.example(
+    [{"genotype_allele_ratio": 0.24, "variant_quality": 101}], {"qual": 100}, False
+)  # Should disregard allele ratio when not set in filter config
+@ht.example(
+    [{"genotype_allele_ratio": 0.24, "variant_quality": None}], {"qual": 100}, False
 )  # Should not filter out alleles with QUAL null
 @ht.example(
-    [{"ar": None, "qual": 99}], {"allele_ratio": 0.25}, False
+    [{"genotype_allele_ratio": None, "variant_quality": 99}], {"allele_ratio": 0.25}, False
 )  # Should not filter out alleles with AR null
 @ht.example(
-    [{"ar": 0.0, "qual": 99}], {"allele_ratio": 0.25}, False
+    [{"genotype_allele_ratio": 0.0, "variant_quality": 99}], {"allele_ratio": 0.25}, False
 )  # Should not filter out alleles with AR==0.0
 @ht.given(st.lists(filter_data(), min_size=1), st.one_of(filter_config()), st.just(None))
 @ht.settings(deadline=500)
@@ -132,16 +154,20 @@ def test_classificationfilter(session, genotype_data, fc, manually_curated_resul
             assert set(result) == set()
 
     expected_result = []
-    qual_threshold = fc.get("qual")
-    ar_threshold = fc.get("allele_ratio")
     for allele_id, gt_data in allele_genotype_data.items():
-        if (
-            qual_threshold is None
-            or (gt_data["qual"] is not None and gt_data["qual"] < qual_threshold)
-        ) and (
-            ar_threshold is None
-            or (gt_data["ar"] is not None and 0.0 < gt_data["ar"] < ar_threshold)
+        if "qual" in fc and (
+            gt_data["variant_quality"] is None or gt_data["variant_quality"] >= fc["qual"]
         ):
-            expected_result.append(allele_id)
+            # Variant quality None or too high
+            continue
+        if "allele_ratio" in fc and (
+            gt_data["genotype_allele_ratio"] is None
+            or gt_data["genotype_allele_ratio"] == 0.0
+            or gt_data["genotype_allele_ratio"] >= fc["allele_ratio"]
+        ):
+            # Allele ratio None, 0.0 or too high
+            continue
+
+        expected_result.append(allele_id)
 
     assert set(result) == set(expected_result)
