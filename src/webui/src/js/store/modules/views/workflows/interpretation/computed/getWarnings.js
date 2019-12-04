41c0f3e7d14d0dc51e74635c5a129c56a24eabee
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2019-12-04 08:23:34 +0100

    [api/webui] Add warning when alleleassessment made by different user group

--- src/webui/src/js/store/modules/views/workflows/interpretation/computed/getWarnings.js
+++ src/webui/src/js/store/modules/views/workflows/interpretation/computed/getWarnings.js
@@ -2,40 +2,56 @@ import { Compute } from 'cerebral'
 import { state } from 'cerebral/tags'
 
 export default (allele) => {
-    return Compute(allele, state`views.workflows.data.collisions`, (allele, collisions, get) => {
-        if (!allele) {
-            return []
-        }
+    return Compute(
+        allele,
+        state`views.workflows.data.collisions`,
+        state`app.user`,
+        (allele, collisions, user, get) => {
+            if (!allele || !user) {
+                return []
+            }
 
-        let warnings = []
-        if (collisions) {
-            for (const c of collisions.filter((c) => c.allele_id === allele.id)) {
-                const typeText =
-                    c.type === 'analysis'
-                        ? `in another analysis: ${c.analysis_name}`
-                        : 'in variant workflow'
-                const unfinishedTypeText =
-                    c.type === 'analysis' ? `an analysis: ${c.analysis_name}` : 'a variant workflow'
+            let warnings = []
+            if (collisions) {
+                for (const c of collisions.filter((c) => c.allele_id === allele.id)) {
+                    const typeText =
+                        c.type === 'analysis'
+                            ? `in another analysis: ${c.analysis_name}`
+                            : 'in variant workflow'
+                    const unfinishedTypeText =
+                        c.type === 'analysis'
+                            ? `an analysis: ${c.analysis_name}`
+                            : 'a variant workflow'
 
-                if (c.user) {
+                    if (c.user) {
+                        warnings.push({
+                            warning: `This variant is currently being worked on by ${c.user.full_name} ${typeText}.`
+                        })
+                    } else {
+                        warnings.push({
+                            warning: `This variant is currently waiting in ${c.workflow_status.toUpperCase()} in ${unfinishedTypeText}.`
+                        })
+                    }
+                }
+
+                if (allele.warnings) {
+                    warnings = warnings.concat(
+                        Object.values(allele.warnings).map((w) => {
+                            return { warning: w }
+                        })
+                    )
+                }
+
+                if (
+                    allele.allele_assessment &&
+                    user.group.name != allele.allele_assessment.usergroup.name
+                ) {
                     warnings.push({
-                        warning: `This variant is currently being worked on by ${c.user.full_name} ${typeText}.`
-                    })
-                } else {
-                    warnings.push({
-                        warning: `This variant is currently waiting in ${c.workflow_status.toUpperCase()} in ${unfinishedTypeText}.`
+                        warning: `This variant's existing classification was performed by a different user group: ${allele.allele_assessment.usergroup.name}.`
                     })
                 }
+                return warnings
             }
         }
-
-        if (allele.warnings) {
-            warnings = warnings.concat(
-                Object.values(allele.warnings).map((w) => {
-                    return { warning: w }
-                })
-            )
-        }
-        return warnings
-    })
+    )
 }
