215fde44b7374f41d8f628dcf646807d984144ea
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2020-02-06 15:22:52 +0100

    [webui] Add auto-scroll-to-top behavior to workflow

--- src/webui/src/js/widgets/scrollIntoView.directive.js
+++ src/webui/src/js/widgets/scrollIntoView.directive.js
@@ -3,10 +3,24 @@ import app from '../ng-decorators'
 app.directive('scrollIntoView', function() {
     return {
         restrict: 'A',
-        link: (scope, elem) => {
-            setTimeout(() => {
-                elem[0].scrollIntoView()
-            }, 500)
+        link: (scope, elem, attrs) => {
+            const getScrollFunc = (elem) => {
+                if (attrs.scrollFunction === 'scrollTo') {
+                    return () => elem.scrollTo({ top: 0, left: 0 })
+                } else {
+                    return () => elem.scrollIntoView()
+                }
+            }
+            if (attrs.scrollOnChange !== undefined) {
+                attrs.$observe('scrollOnChange', () => {
+                    getScrollFunc(elem[0])()
+                })
+            } else {
+                // Auto scroll upon creation
+                setTimeout(() => {
+                    getScrollFunc(elem[0])()
+                }, 500)
+            }
         }
     }
 })
