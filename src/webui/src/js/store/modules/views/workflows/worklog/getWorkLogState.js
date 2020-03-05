7edb37085bb740c22c90db99a1f868b44166af38
Svein Tore Koksrud Seljebotn <s.t.seljebotn@medisin.uio.no>
2020-03-05 15:05:21 +0100

    [webui] Change default in work log to "Messages only"

--- src/webui/src/js/store/modules/views/workflows/worklog/getWorkLogState.js
+++ src/webui/src/js/store/modules/views/workflows/worklog/getWorkLogState.js
@@ -4,6 +4,6 @@ export default function getWorkLogState() {
         messageIds: null,
         messageCount: null,
         messages: null,
-        showMessagesOnly: false
+        showMessagesOnly: true
     }
 }
