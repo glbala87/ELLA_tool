540392fcd2cc5e0b608d540988e1503d80b345ae
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-03-17 12:29:32 +0100

    [ops] Create template for production supervisor config

--- ops/prod/entrypoint.sh
+++ ops/prod/entrypoint.sh
@@ -5,7 +5,25 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Abort if not migrated
 ella-cli database compare
 
+SUPERVISOR_USERNAME=${SUPERVISOR_USERNAME:-ella}
+SUPERVISOR_PASSWORD=${SUPERVISOR_USERNAME:-alleles}
+
+SUPERVISOR_CONFIG=${SUPERVISOR_CONFIG:-${DIR}/supervisor.cfg}
+envsubst < supervisor-template.cfg | python -c '
+import sys
+import os
+from configparser import ConfigParser
+config = ConfigParser(interpolation=None)
+config.readfp(sys.stdin)
+if not "SUPERVISOR_PORT" in os.environ:
+    config.pop("inet_http_server")
+for section in config.values():
+    [section.pop(k) for k,v in section.items() if not v]
+
+config.write(sys.stdout)
+' > ${SUPERVISOR_CONFIG}
+
 export PORT=${PORT:-3114}
 
 # Start services
-exec supervisord -c ${DIR}/supervisor.cfg
+exec supervisord -c ${SUPERVISOR_CONFIG}
