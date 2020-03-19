46a43fe30d814537e06a3302c8e7e139b15a3622
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-03-19 12:38:49 +0100

    [api/db/ops] Use pathlib rather than os.path

--- src/applogger.py
+++ src/applogger.py
@@ -1,12 +1,12 @@
-import os
 import logging
 import logging.config
 import yaml
+from pathlib import Path
 
-SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+SCRIPT_DIR = Path(__file__).absolute().parent
 
 
 def setup_logger():
-    with open(os.path.join(SCRIPT_DIR, "logconfig.yaml"), "r") as file:
+    with open(SCRIPT_DIR / "logconfig.yaml", "r") as file:
         config = yaml.safe_load(file.read())
         logging.config.dictConfig(config)
