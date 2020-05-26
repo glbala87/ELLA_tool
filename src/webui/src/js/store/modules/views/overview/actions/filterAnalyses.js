abb0e883d368d44f8d0c493355bbea46ded06619
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2020-05-26 13:05:33 +0200

    [webui] all filters working

--- src/webui/src/js/store/modules/views/overview/actions/filterAnalyses.js
+++ src/webui/src/js/store/modules/views/overview/actions/filterAnalyses.js
@@ -20,7 +20,7 @@ export default function filterAnalyses({ state }) {
                         (filter.priorityNormal && a.priority == 1) ||
                         (filter.priorityHigh && a.priorty == 2) ||
                         (filter.priorityUrgent && a.priority == 3)) &&
-                    (!filter.dateRange || withinRange(filter.dateRange, a.date_requested))
+                    (!filter.dateRange || withinRange(filter.dateRange, new Date(a.date_requested)))
                 )
             })
         }
@@ -30,12 +30,47 @@ export default function filterAnalyses({ state }) {
 }
 
 function withinRange(filter, date) {
-    const [amount, unit] = filter.split(':')
-    // let filterDate
-    // if (unit == "d") {
-    //     filterDate = new Date(a.)
-    // } else if (unit == "") {
+    // filter: (lt|le|gt|ge):Int:(d|m)
+    const [op, amount, unit] = filter.split(':')
+    let filterDate
+    if (unit == 'd') {
+        filterDate = createDate(Number(amount), 0)
+    } else if (unit == 'm') {
+        filterDate = createDate(0, Number(amount))
+    } else {
+        throw new SyntaxError(`Invalid unit in filter string: ${filter}`)
+    }
 
-    // }
-    return true
+    // only using lt, ge now, but might as well support others
+    if (op == 'lt') {
+        return filterDate < date
+    } else if (op == 'le') {
+        return filterDate <= date
+    } else if (op == 'gt') {
+        return filterDate > date
+    } else if (op == 'ge') {
+        return filterDate >= date
+    } else {
+        throw new SyntaxError(`Invalid operation in filter string: ${filter}`)
+    }
+}
+
+function createDate(days, months) {
+    const orig_date = new Date()
+    const date = new Date(orig_date.getTime())
+    date.setHours(0)
+    date.setMinutes(0)
+    date.setSeconds(0)
+    date.setMilliseconds(0)
+    date.setDate(date.getDate() + days)
+    date.setMonth(date.getMonth() + months)
+
+    // time is hard and js month math is bad, e.g.
+    // date = new Date(2020, 2, 30) -> Mon Mar 30 2020 00:00:00
+    // date.setMonth(date.getMonth() - 1) -> Sun Mar 01 2020 00:00:00
+    // So we check what the real month delta is and adjust days as necessary
+    while ((orig_date.getMonth() - date.getMonth() + 12) % 12 != Math.abs(months)) {
+        date.setDate(date.getDate() - 1)
+    }
+    return date
 }
