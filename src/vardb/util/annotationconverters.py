f3e548c245e06a267ea32d152bafafe417f1db0b
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-06-11 14:32:49 +0200

    [db] Fetch clinvar references from 'pubmed_ids' in the clinvarjson

--- src/vardb/util/annotationconverters.py
+++ src/vardb/util/annotationconverters.py
@@ -679,6 +679,7 @@ class ConvertReferences(object):
         )
 
         pubmeds = clinvarjson.get("pubmeds", [])
+        pubmeds += clinvarjson.get("pubmed_ids", [])
         pubmeds = dict(list(zip(pubmeds, [""] * len(pubmeds))))  # Return as dict (empty values)
 
         return pubmeds
