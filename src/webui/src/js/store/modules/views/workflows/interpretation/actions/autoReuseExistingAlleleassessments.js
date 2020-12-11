a068170580ce32a551f05a64fcf45f616807fc82
Øyvind Evju <oyvind.evju@medisin.uio.no>
2020-12-11 09:48:33 +0100

    [webui] Issue toast for variants in a non-inital state with updated allele assessment

--- src/webui/src/js/store/modules/views/workflows/interpretation/actions/autoReuseExistingAlleleassessments.js
+++ src/webui/src/js/store/modules/views/workflows/interpretation/actions/autoReuseExistingAlleleassessments.js
@@ -1,23 +1,31 @@
+import { deepEquals } from '../../../../../../util'
+import { prepareAlleleAssessmentModel } from '../../../../../common/helpers/alleleState'
 import getAlleleState from '../computed/getAlleleState'
 
 export default function autoReuseExistingAlleleassessments({ state, resolve }) {
     const alleles = state.get('views.workflows.interpretation.data.alleles')
     const checkReportAlleleIds = []
     const updatedAlleleAssessmentAlleleIds = []
+    const defaultAlleleAssessmentModel = {}
+    prepareAlleleAssessmentModel(defaultAlleleAssessmentModel)
 
     for (let [alleleId, allele] of Object.entries(alleles)) {
         if (!allele.allele_assessment) {
             continue
         }
+
         const alleleState = resolve.value(getAlleleState(alleleId))
+
+        const alleleassessmentStateIsInital =
+            deepEquals(alleleState.alleleassessment, defaultAlleleAssessmentModel) ||
+            deepEquals(alleleState.alleleassessment, {})
 
         const hasNewAlleleAssessment =
             !('reuseCheckedId' in alleleState.alleleassessment) ||
             alleleState.alleleassessment.reuseCheckedId < allele.allele_assessment.id
-
-        const hasUpdatedAlleleAssessment =
-            'reuseCheckedId' in alleleState.alleleassessment &&
-            alleleState.alleleassessment.reuseCheckedId < allele.allele_assessment.id
+        // Check if allele assessment has been updated, i.e. if the current state is not in its inital state
+        // Need to check this to avoid toast messages for all classified alleles when opening an analysis for the first time
+        const hasUpdatedAlleleAssessment = hasNewAlleleAssessment && !alleleassessmentStateIsInital
 
         const isReused = alleleState.alleleassessment.reuse
 
