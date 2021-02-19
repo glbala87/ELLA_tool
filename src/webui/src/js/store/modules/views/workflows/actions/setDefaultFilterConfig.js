3fa8400c424323cef2d5681474d130a23450e0d6
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-02-19 12:30:31 +0100

    [web] Fallback to default filterconfig if no filterconfig set for finalized analyses

--- src/webui/src/js/store/modules/views/workflows/actions/setDefaultFilterConfig.js
+++ src/webui/src/js/store/modules/views/workflows/actions/setDefaultFilterConfig.js
@@ -10,10 +10,18 @@ export default function setDefaultFilterConfig({ state, resolve }) {
     const current = state.get('views.workflows.interpretation.selectedId') === 'current'
     const isDone = selectedInterpretation.status === 'Done'
 
-    // Only set default filter config when showing current data or in an ongoing interpretation,
+    // Set default filter config when showing current data or in an ongoing interpretation,
     // and the selected filter config is not available for filtering
-    if ((current || !isDone) && !availableFilterConfigIds.includes(stateFilterConfigId)) {
+    // OR interpretation is done, but filterconfigId is not set in state (historical data only)
+    if (
+        ((current || !isDone) && !availableFilterConfigIds.includes(stateFilterConfigId)) ||
+        (isDone && !stateFilterConfigId)
+    ) {
         let defaultFilterConfigId = state.get('views.workflows.data.filterconfigs')[0].id
         state.set('views.workflows.interpretation.state.filterconfigId', defaultFilterConfigId)
+    }
+
+    if (!state.get('views.workflows.interpretation.state.filterconfigId')) {
+        throw Error('Filter config ID not set in state. This should not be possible')
     }
 }
