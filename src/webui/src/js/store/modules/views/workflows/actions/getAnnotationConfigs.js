2e460702f3fcc5c0c646506ea131fcc5efda86de
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-05-20 15:55:53 +0200

    [web] Change some actions/computes into factory functions, required for add excluded modal

--- src/webui/src/js/store/modules/views/workflows/actions/getAnnotationConfigs.js
+++ src/webui/src/js/store/modules/views/workflows/actions/getAnnotationConfigs.js
@@ -1,21 +1,27 @@
-function getAnnotationConfigs({ http, path, state }) {
-    const alleles = state.get(`views.workflows.interpretation.data.alleles`)
-    if (!alleles) {
-        return path.success({})
-    }
+const getAnnotationConfigs = (alleles) => {
+    return ({ http, path, resolve }) => {
+        alleles = resolve.value(alleles)
+        if (!alleles || !Object.keys(alleles).length) {
+            return path.success({ result: [] })
+        }
 
-    const annotationConfigIds = Object.values(alleles).map((x) => x.annotation.annotation_config_id)
-
-    return http
-        .get(
-            `annotationconfigs/?annotation_config_ids=${Array.from(annotationConfigIds).join(',')}`
+        const annotationConfigIds = new Set(
+            Object.values(alleles).map((x) => x.annotation.annotation_config_id)
         )
-        .then((response) => {
-            return path.success({ result: response.result })
-        })
-        .catch((response) => {
-            return path.error({ result: response.result })
-        })
+
+        return http
+            .get(
+                `annotationconfigs/?annotation_config_ids=${Array.from(annotationConfigIds).join(
+                    ','
+                )}`
+            )
+            .then((response) => {
+                return path.success({ result: response.result })
+            })
+            .catch((response) => {
+                return path.error({ result: response.result })
+            })
+    }
 }
 
 export default getAnnotationConfigs
