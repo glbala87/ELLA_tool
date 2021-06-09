58074ad8542fb79cc0732bda84eee6c6c1da8401
Florian Krull <flo.krull@gmail.com>
2021-06-09 12:48:59 +0200

    [web] views/visualization fix reodrer

--- src/webui/src/js/views/visualization.directive.js
+++ src/webui/src/js/views/visualization.directive.js
@@ -6,6 +6,41 @@ import { state, signal } from 'cerebral/tags'
 import { Compute } from 'cerebral'
 import template from './visualization.ngtmpl.html' // eslint-disable-line no-unused-vars
 
+// array of unique preset ids
+const _getPresetIds = (tracks) => {
+    return Compute(tracks, (tracks) => Object.keys(_getPresetTracks(tracks)))
+}
+
+const _getCurrPresetModel = (tracks) => {
+    return Compute(tracks, (tracks) => {
+        // set of track IDs: Set[track_ID_1, track_ID_2, ... ]
+        const _getSelectedTracks = (tracks) => {
+            const r = new Set()
+            if (!tracks) {
+                return r
+            }
+            Object.keys(tracks).forEach((trackCatId) => {
+                tracks[trackCatId].forEach((track, trackIdx) => {
+                    if (track.selected) {
+                        r.add(_getTrackId(trackCatId, trackIdx))
+                    }
+                })
+            })
+            return r
+        }
+        const _equalSet = (a, b) => a.size === b.size && [...a].every((value) => b.has(value))
+        const presetTracks = _getPresetTracks(tracks)
+        const currTrackSelection = _getSelectedTracks(tracks)
+        for (let presetId of Object.keys(presetTracks)) {
+            if (_equalSet(currTrackSelection, presetTracks[presetId])) {
+                return presetId
+            }
+        }
+        // no preset selected
+        return null
+    })
+}
+
 app.component('visualization', {
     templateUrl: 'visualization.ngtmpl.html',
     controller: connect(
@@ -66,41 +101,6 @@ const _getPresetTracks = (tracks) => {
         })
     })
     return r
-}
-
-// array of unique preset ids
-const _getPresetIds = (tracks) => {
-    return Compute(tracks, (tracks) => Object.keys(_getPresetTracks(tracks)))
-}
-
-const _getCurrPresetModel = (tracks) => {
-    return Compute(tracks, (tracks) => {
-        // set of track IDs: Set[track_ID_1, track_ID_2, ... ]
-        const _getSelectedTracks = (tracks) => {
-            const r = new Set()
-            if (!tracks) {
-                return r
-            }
-            Object.keys(tracks).forEach((trackCatId) => {
-                tracks[trackCatId].forEach((track, trackIdx) => {
-                    if (track.selected) {
-                        r.add(_getTrackId(trackCatId, trackIdx))
-                    }
-                })
-            })
-            return r
-        }
-        const _equalSet = (a, b) => a.size === b.size && [...a].every((value) => b.has(value))
-        const presetTracks = _getPresetTracks(tracks)
-        const currTrackSelection = _getSelectedTracks(tracks)
-        for (let presetId of Object.keys(presetTracks)) {
-            if (_equalSet(currTrackSelection, presetTracks[presetId])) {
-                return presetId
-            }
-        }
-        // no preset selected
-        return null
-    })
 }
 
 const _updateTrackSelections = (tracks, setectedPresetId, shownTracksChanged) => {
