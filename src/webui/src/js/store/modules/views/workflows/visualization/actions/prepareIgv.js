2c793d2d9c95d7a711716de736aa6d02bec120e4
Florian Krull <flo.krull@gmail.com>
2021-06-11 10:57:46 +0200

    [web] prepareIgv sep igv config from ella cfg

--- src/webui/src/js/store/modules/views/workflows/visualization/actions/prepareIgv.js
+++ src/webui/src/js/store/modules/views/workflows/visualization/actions/prepareIgv.js
@@ -11,78 +11,84 @@ export default async function prepareIgv({ state, http }) {
     const analysis = state.get('views.workflows.data.analysis')
     const alleles = state.get('views.workflows.interpretation.data.alleles')
 
-    const result = await http.get(`igv/tracks/${analysis.id}`)
-    const tracks = {
+    const dynamicTracksResult = await http.get(`igv/tracks/${analysis.id}`)
+    const predefinedTracks = {
         global: [
             {
                 id: 'genepanel',
                 selected: false,
-                config: {
-                    name: 'Genepanel',
-                    type: 'annotation',
-                    url: `/api/v1/igv/genepanel/${analysis.genepanel.name}/${analysis.genepanel.version}/`,
-                    format: 'bed',
-                    indexed: false,
-                    displayMode: 'EXPANDED',
-                    order: 10,
-                    height: 60,
-                    presets: ['Test 1', 'Test 2']
-                }
+                show: false,
+                name: 'Genepanel',
+                type: 'annotation',
+                url: `/api/v1/igv/genepanel/${analysis.genepanel.name}/${analysis.genepanel.version}/`,
+                format: 'bed',
+                indexed: false,
+                displayMode: 'EXPANDED',
+                order: 10,
+                height: 60,
+                presets: ['Test 1', 'Test 2']
             },
             {
                 id: 'classifications',
-                selected: false,
-                config: {
-                    name: 'Classifications',
-                    url: '/api/v1/igv/classifications/',
-                    format: 'bed',
-                    indexed: false,
-                    order: 11,
-                    visibilityWindow: Number.MAX_VALUE,
-                    presets: ['Test 1', 'Test 3']
-                }
+                show: false,
+                name: 'Classifications',
+                url: '/api/v1/igv/classifications/',
+                format: 'bed',
+                indexed: false,
+                order: 11,
+                visibilityWindow: Number.MAX_VALUE,
+                presets: ['Test 1', 'Test 3']
             }
         ],
-        user: [],
         analysis: [
             {
                 id: 'variants',
-                selected: true,
-                config: {
-                    name: 'Variants',
-                    url: `/api/v1/igv/variants/${analysis.id}/?allele_ids=${Object.keys(
-                        alleles
-                    ).join(',')}`,
-                    format: 'vcf',
-                    indexed: false,
-                    order: 12,
-                    visibilityWindow: Number.MAX_VALUE,
-                    presets: ['Test 3']
-                }
+                show: true,
+                name: 'Variants',
+                url: `/api/v1/igv/variants/${analysis.id}/?allele_ids=${Object.keys(alleles).join(
+                    ','
+                )}`,
+                format: 'vcf',
+                indexed: false,
+                order: 12,
+                visibilityWindow: Number.MAX_VALUE,
+                presets: ['Test 3']
             }
         ]
     }
 
-    for (const [category, categoryTracks] of Object.entries(result.result)) {
-        for (const track of categoryTracks) {
-            tracks[category].push({
-                id: track.id,
-                selected: 'show' in track ? track.show : true,
-                config: track
-            })
-        }
-        tracks[category].sort(thenBy((t) => t.config.order || 99999))
-    }
-    Object.values(tracks).forEach((trackCategory) => {
-        trackCategory.forEach((track) => {
-            if (track.selected) {
-                const presetIdDefault = 'Default'
-                track.config.presets =
-                    track.config.presets !== undefined
-                        ? [presetIdDefault, ...track.config.presets]
-                        : [presetIdDefault]
+    const _appendFinalizedTracks = (tracks, cfgTracks) => {
+        for (const [category, categoryTracks] of Object.entries(cfgTracks)) {
+            for (const track of categoryTracks) {
+                const finalizedTrack = {
+                    id: track.id,
+                    selected: 'show' in track ? track.show : false,
+                    config: track,
+                    presets: track.presets !== undefined ? track.presets : []
+                }
+                // cleanup
+                delete finalizedTrack.config.show
+                delete finalizedTrack.config.presets
+                // derive default preset
+                if (finalizedTrack.selected) {
+                    const presetIdDefault = 'Default'
+                    finalizedTrack.presets = [presetIdDefault, ...finalizedTrack.presets]
+                }
+                // append
+                if (!tracks.hasOwnProperty(category)) {
+                    tracks[category] = []
+                }
+                tracks[category].push(finalizedTrack)
             }
-        })
-    })
-    state.set('views.workflows.visualization.tracks', tracks)
+        }
+    }
+
+    const finalizedTracks = {}
+    _appendFinalizedTracks(finalizedTracks, predefinedTracks)
+    _appendFinalizedTracks(finalizedTracks, dynamicTracksResult.result)
+
+    for (const categoryTracks of Object.values(finalizedTracks)) {
+        categoryTracks.sort(thenBy((t) => t.config.order || 99999))
+    }
+    state.set('views.workflows.visualization.tracks', finalizedTracks)
 }
