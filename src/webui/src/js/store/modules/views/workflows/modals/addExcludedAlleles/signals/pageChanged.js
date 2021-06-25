d030f5cf6f48efccfa379a44452098399d01897d
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-06-25 13:07:09 +0200

    [web] Reload annotation configs when excluded alleles change. Minor fixes.

--- src/webui/src/js/store/modules/views/workflows/modals/addExcludedAlleles/signals/pageChanged.js
+++ src/webui/src/js/store/modules/views/workflows/modals/addExcludedAlleles/signals/pageChanged.js
@@ -1,13 +1,15 @@
 import { set, debounce } from 'cerebral/operators'
 import { state, props } from 'cerebral/tags'
 import loadExcludedAlleles from '../sequences/loadExcludedAlleles'
+import loadAnnotationConfigs from '../sequences/loadAnnotationConfigs'
 
 export default [
     debounce(300),
     {
         continue: [
             set(state`views.workflows.modals.addExcludedAlleles.selectedPage`, props`selectedPage`),
-            loadExcludedAlleles
+            loadExcludedAlleles,
+            loadAnnotationConfigs
         ],
         discard: []
     }
