60166a7667ef956a00361246da68c79128f5b65d
scalavision <scalavision@gmail.com>
2021-07-01 13:48:32 +0200

    [56-cnv-minimal-rc1] reformatting and refactoring

--- src/webui/src/js/store/modules/views/workflows/visualization/actions/updateIgvLocus.js
+++ src/webui/src/js/store/modules/views/workflows/visualization/actions/updateIgvLocus.js
@@ -1,16 +1,14 @@
 export default function updateIgvLocus({ state, props }) {
     const allele = state.get(`views.workflows.interpretation.data.alleles.${props.alleleId}`)
 
-    /* This is in dev now
     const N = 50
-    const igvLocus = {
-        chr: allele.chromosome,
-        pos: allele.vcf_pos
-    }*/
-
-    const padding = Math.max(50 / 2, 0.1 * allele.length)
+    const padding = Math.max(N / 2, 0.1 * allele.length)
     const start = Math.max(allele.vcf_pos - padding, 0)
     const end = allele.vcf_pos + allele.length + padding
-    const igvLocus = `${allele.chromosome}:${start}-${Math.abs(end)}`
+    const igvLocus = {
+        chr: allele.chromosome,
+        pos: `${start}-${Math.abs(end)}`
+    }
+
     state.set('views.workflows.visualization.igv.locus', igvLocus)
 }
