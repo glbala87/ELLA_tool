48783dfa8679ac7e370e3ce2ea40ad2f22bec351
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-07-08 09:29:55 +0200

    [web] Fix error that caused popups to sometimes fail in addExcludedModal

--- src/webui/src/js/store/modules/views/workflows/computed/getAnnotationConfigItem.js
+++ src/webui/src/js/store/modules/views/workflows/computed/getAnnotationConfigItem.js
@@ -3,14 +3,29 @@ import { state, props } from 'cerebral/tags'
 
 export default Compute(
     state`views.workflows.data.annotationConfigs`,
+    state`views.workflows.modals.addExcludedAlleles.data.annotationConfigs`,
     props`annotationConfigId`,
     props`annotationConfigItemIdx`,
-    (annotationConfigs, annotationConfigId, annotationConfigItemIdx) => {
+    (
+        annotationConfigs,
+        excludedModalAnnotationConfigs,
+        annotationConfigId,
+        annotationConfigItemIdx
+    ) => {
         if (annotationConfigs === undefined) {
             return null
         }
-        return annotationConfigs.find((x) => x.id === annotationConfigId).view[
-            annotationConfigItemIdx
-        ].config
+
+        // If annotationConfig is not in the annotationConfigs array, this must mean that we are
+        // looking at popups in the addExcludedModal (which might have a different set of annotation configs
+        // loaded). It is irrelevant which state source is used to find the config.
+        let annotationConfig = annotationConfigs.find((x) => x.id === annotationConfigId)
+
+        if (!annotationConfig) {
+            annotationConfig = excludedModalAnnotationConfigs.find(
+                (x) => x.id === annotationConfigId
+            )
+        }
+        return annotationConfig.view[annotationConfigItemIdx].config
     }
 )
