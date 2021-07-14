ede99373dc79157e4d87968cf38fbb9356da1018
Florian Krull <flo.krull@gmail.com>
2021-07-14 15:38:19 +0200

    [test] add e2e test for empty analysis

---
+++ src/webui/tests/e2e/tests/workflow_sample_interpretation_empty.js
@@ -0,0 +1,94 @@
+require('core-js/fn/object/entries')
+
+/**
+ * Make sure an empty analysis loads, can be started, moved to different
+ * workflow step ("Finish", send to e.g. "Review") and finalized.
+ * Test sections: INFO, CLASSIFICATION and REPORT.
+ */
+
+let LoginPage = require('../pageobjects/loginPage')
+let SampleSelection = require('../pageobjects/overview_samples')
+let AnalysisPage = require('../pageobjects/analysisPage')
+
+let loginPage = new LoginPage()
+let overview = new SampleSelection()
+let analysisPage = new AnalysisPage()
+
+const TITLE_INTERPRETATION = ' • INTERPRETATION'
+const TITLE_REVIEW = ' • REVIEW'
+const TITLE_MEDICAL_REVIEW = ' • MEDICAL REVIEW'
+const ANALYSIS_NAME = 'brca_e2e_test04.HBOCUTV_v01'
+
+describe('Sample workflow with an empty anaysis', function() {
+    beforeAll(() => {
+        browser.resetDb()
+    })
+
+    it(`sets empty analysis (${ANALYSIS_NAME}) to review`, function() {
+        loginPage.open()
+        loginPage.loginAs('testuser1')
+        overview.open()
+        overview.selectPending(4)
+        expect(analysisPage.title).toBe(ANALYSIS_NAME + TITLE_INTERPRETATION)
+        console.log('Changing to report page')
+        analysisPage.selectSectionReport()
+        console.log('Changing to info page')
+        analysisPage.selectSectionInfo()
+        console.log('Changing to classification page')
+        analysisPage.selectSectionClassification()
+        analysisPage.startButton.click()
+        analysisPage.finishButton.click()
+        analysisPage.markReviewButton.click()
+        analysisPage.modalFinishButton.click()
+    })
+
+    it(`sets empty analysis (${ANALYSIS_NAME}) to medical review`, function() {
+        loginPage.open()
+        loginPage.loginAs('testuser1')
+        overview.open()
+        overview.selectTopReview()
+        expect(analysisPage.title).toBe(ANALYSIS_NAME + TITLE_REVIEW)
+        console.log('Changing to report page')
+        analysisPage.selectSectionReport()
+        console.log('Changing to info page')
+        analysisPage.selectSectionInfo()
+        console.log('Changing to classification page')
+        analysisPage.selectSectionClassification()
+        analysisPage.startButton.click()
+        analysisPage.finishButton.click()
+        analysisPage.markMedicalReviewButton.click()
+        analysisPage.modalFinishButton.click()
+    })
+
+    it(`sets empty analysis (${ANALYSIS_NAME}) to finalized`, function() {
+        loginPage.open()
+        loginPage.loginAs('testuser1')
+        overview.open()
+        overview.selectTopMedicalReview()
+        expect(analysisPage.title).toBe(ANALYSIS_NAME + TITLE_MEDICAL_REVIEW)
+        console.log('Changing to report page')
+        analysisPage.selectSectionReport()
+        console.log('Changing to info page')
+        analysisPage.selectSectionInfo()
+        console.log('Changing to classification page')
+        analysisPage.selectSectionClassification()
+        analysisPage.startButton.click()
+        analysisPage.finishButton.click()
+        analysisPage.finalizeButton.click()
+        analysisPage.modalFinishButton.click()
+    })
+
+    it(`select empty finished analysis (${ANALYSIS_NAME})`, function() {
+        loginPage.open()
+        loginPage.loginAs('testuser1')
+        overview.open()
+        overview.selectFinished(1)
+        expect(analysisPage.title).toBe(ANALYSIS_NAME + TITLE_MEDICAL_REVIEW)
+        console.log('Changing to report page')
+        analysisPage.selectSectionReport()
+        console.log('Changing to info page')
+        analysisPage.selectSectionInfo()
+        console.log('Changing to classification page')
+        analysisPage.selectSectionClassification()
+    })
+})
