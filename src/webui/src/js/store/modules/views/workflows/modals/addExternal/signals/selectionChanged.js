9f878cb89d6b621c08125f78e425df0f34a334f0
Florian Krull <flo.krull@gmail.com>
2021-08-17 21:54:05 +0200

    [web] modals/addExternal fix setting 'undefined' in state

--- src/webui/src/js/store/modules/views/workflows/modals/addExternal/signals/selectionChanged.js
+++ src/webui/src/js/store/modules/views/workflows/modals/addExternal/signals/selectionChanged.js
@@ -1,6 +1,11 @@
 import { set } from 'cerebral/operators'
-import { state, props } from 'cerebral/tags'
 
 export default [
-    set(state`views.workflows.modals.addExternal.selection.${props`key`}`, props`value`)
+    ({ state, props }) => {
+        const { key, value } = props
+        state.set(
+            `views.workflows.modals.addExternal.selection.${key}`,
+            value === undefined || value == '' ? null : value
+        )
+    }
 ]
