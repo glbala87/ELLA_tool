0627ad6b3dc1c8fedc35b85cb575862f809f146f
Florian Krull <flo.krull@gmail.com>
2021-08-18 12:47:59 +0200

    [web] POST customAnnotationdata/: remove all null fields from groups

--- src/webui/src/js/store/modules/views/workflows/interpretation/actions/postCustomAnnotation.js
+++ src/webui/src/js/store/modules/views/workflows/interpretation/actions/postCustomAnnotation.js
@@ -1,7 +1,22 @@
+import { deepCopy } from '../../../../../../util'
+
 export default function postCustomAnnotation({ state, http, path, props }) {
+    const customAnnotationDataPruned = deepCopy(props.customAnnotationData)
+    // filter - use only entries that have non-null sub-properties
+    //   example: `{prediction: {ortholog_conservation: null, dna_conservation: null}}`
+    //        =>  `{prediction: {}}`
+    Object.keys(customAnnotationDataPruned).forEach((grpk) => {
+        customAnnotationDataPruned[grpk] = Object.entries(customAnnotationDataPruned[grpk])
+            .filter(([k, v]) => v !== null)
+            .reduce((obj, [k, v]) => {
+                obj[k] = v
+                return obj
+            }, {})
+    })
+
     const payload = {
         allele_id: props.alleleId,
-        annotations: props.customAnnotationData,
+        annotations: customAnnotationDataPruned,
         user_id: state.get('app.user.id')
     }
 
