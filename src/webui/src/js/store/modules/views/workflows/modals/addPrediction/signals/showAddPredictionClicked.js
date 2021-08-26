7008a604c2da8473a0767b3ce6e18ce8f60841a0
Florian Krull <flo.krull@gmail.com>
2021-08-26 13:53:33 +0200

    [web] showAddPredictionClicked avoid POSTing non-custom annotation

--- src/webui/src/js/store/modules/views/workflows/modals/addPrediction/signals/showAddPredictionClicked.js
+++ src/webui/src/js/store/modules/views/workflows/modals/addPrediction/signals/showAddPredictionClicked.js
@@ -8,15 +8,22 @@ export default [
         const annotation = state.get(
             `views.workflows.interpretation.data.alleles.${alleleId}.annotation`
         )
-        const annotationExt = annotation.hasOwnProperty('prediction')
+        const annotationGroups = state.get('app.config.custom_annotation.prediction')
+        // fetch the annotation from the state and remove keys that are not in the group
+        //   to avoid POSTing non-custom annoation
+        let annotationPred = annotation.hasOwnProperty('prediction')
             ? deepCopy(annotation.prediction)
             : {}
-        state.set(`views.workflows.modals.addPrediction.selection`, annotationExt)
+        const annotationGroupKeys = annotationGroups.map((e) => e.key)
+        annotationPred = Object.entries(annotationPred)
+            .filter(([k, v]) => annotationGroupKeys.includes(k))
+            .reduce((obj, [k, v]) => {
+                obj[k] = v
+                return obj
+            }, {})
+        state.set(`views.workflows.modals.addPrediction.selection`, annotationPred)
+        state.set(`views.workflows.modals.addPrediction.annotationGroups`, annotationGroups)
     },
-    set(
-        state`views.workflows.modals.addPrediction.annotationGroups`,
-        state`app.config.custom_annotation.prediction`
-    ),
     set(state`views.workflows.modals.addPrediction.alleleId`, props`alleleId`),
     set(state`views.workflows.modals.addPrediction.show`, true)
 ]
