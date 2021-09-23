2a8752393b242bdd09549705bad9dc4e45642414
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-09-23 15:55:47 +0200

    [web] Refactor autosize-textarea

--- src/webui/src/js/widgets/autosizeTextarea.directive.js
+++ src/webui/src/js/widgets/autosizeTextarea.directive.js
@@ -1,16 +1,11 @@
 /* jshint esnext: true */
 
-import { Directive } from '../ng-decorators'
 import autosize from 'autosize'
+import app from '../ng-decorators'
+import { connect } from '@cerebral/angularjs'
 
-/**
- <autosize-textarea>
- Autosize textarea element that will automatically resize with user input
- */
-@Directive({
-    selector: 'autosize-textarea',
-    restrict: 'E',
-    scope: {
+app.component('autosizeTextarea', {
+    bindings: {
         placeholder: '@?',
         ngModel: '=',
         ngModelWatch: '=?',
@@ -18,25 +13,38 @@ import autosize from 'autosize'
         ngChange: '&?'
     },
     template:
-        '<textarea class="id-autosizeable" ng-disabled="vm.ngDisabled" rows=1 placeholder="{{vm.placeholder}}" ng-model="vm.ngModel" ng-model-watch="vm.ngModelWatch"></textarea>',
-    link: (scope, elem, attrs) => {
-        let textarea = elem.children()[0]
-        autosize(textarea)
-        scope.$watch(
-            () => scope.ngModel,
-            () => {
-                // Will not work with ng-change in template, probably due to some priority issues
-                if (scope.ngChange) {
-                    scope.ngChange()
-                }
-                autosize.update(textarea)
+        '<textarea class="id-autosizeable" ng-disabled="$ctrl.ngDisabled" rows=1 placeholder="{{$ctrl.placeholder}}" ng-model="$ctrl.ngModel" ng-model-watch="$ctrl.ngModelWatch"></textarea>',
+    controller: connect(
+        {},
+        'autosizeTextarea',
+        [
+            '$scope',
+            '$element',
+            ($scope, $element) => {
+                const $ctrl = $scope.$ctrl
+                const textarea = $element.children()[0]
+                autosize(textarea)
+                $scope.$watch(
+                    () => {
+                        return $ctrl.ngModel
+                    },
+                    (n, o) => {
+                        // ng-change in the template seems to run before ng-model has been set,
+                        // therefore we need to run it here
+                        // Do not trigger ngChange if new type is not yet defined
+                        if ($ctrl.ngChange && n !== undefined) {
+                            $ctrl.ngChange()
+                        }
+                        autosize(textarea)
+                    }
+                )
+
+                // when textarea initially is hidden and then shown, we must explicitly autosize it:
+                // (http://www.jacklmoore.com/autosize/#faq-hidden)
+                setTimeout(() => {
+                    autosize.update(textarea)
+                }, 100)
             }
-        )
-        // when textarea initially is hidden and then shown, we must explicitly autosize it:
-        // (http://www.jacklmoore.com/autosize/#faq-hidden)
-        setTimeout(() => {
-            autosize.update(textarea)
-        }, 100)
-    }
+        ]
+    )
 })
-export class AutosizeTextareaController {}
