ba88b80f61542a7c02453b303ea66c55c6bf43bc
scalavision <scalavision@gmail.com>
2021-09-29 14:13:45 +0200

    fix hgvsg for all allele change_types

--- src/webui/src/js/store/common/helpers/processAlleles.js
+++ src/webui/src/js/store/common/helpers/processAlleles.js
@@ -81,48 +81,50 @@ function getFormatted(allele, genepanel) {
     // (dup: Chr13(GRCh37):g.32912008_3291212dup )
 
     // Database is 0-based, hgvsg uses 1-based index
-    let start = allele.start_position
+    let start = allele.start_position + 1
     let end = allele.open_end_position
-    let cnvEnd = start + 1 + allele.length
+    // converting to 1 based index, closed system.
+    // vcf defined cnv END: POS + length(REF allele) - 1
+    let cnvEnd = start + allele.length - 1
 
     switch (allele.change_type) {
         case 'SNP':
-            formatted.genomicPosition = `${allele.chromosome}:${start + 1}`
-            formatted.hgvsg = `g.${start + 1}${allele.change_from}>${allele.change_to}`
+            formatted.genomicPosition = `${allele.chromosome}:${start}`
+            formatted.hgvsg = `g.${start}${allele.change_from}>${allele.change_to}`
             break
         case 'del':
-            if (start + 1 === end) {
-                formatted.hgvsg = `g.${start + 1}del`
-                formatted.genomicPosition = `${allele.chromosome}:${start + 1}`
+            if (start === end) {
+                formatted.hgvsg = `g.${start}del`
+                formatted.genomicPosition = `${allele.chromosome}:${start}`
             } else {
-                formatted.hgvsg = `g.${start + 1}_${end}del`
-                formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${end}`
+                formatted.hgvsg = `g.${start}_${end}del`
+                formatted.genomicPosition = `${allele.chromosome}:${start}-${end}`
             }
             break
         case 'indel':
-            if (start + 1 === end) {
-                formatted.hgvsg = `g.${start + 1}delins${allele.change_to}`
-                formatted.genomicPosition = `${allele.chromosome}:${start + 1}`
+            if (start === end) {
+                formatted.hgvsg = `g.${start}delins${allele.change_to}`
+                formatted.genomicPosition = `${allele.chromosome}:${start}`
             } else {
-                formatted.hgvsg = `g.${start + 1}_${end}delins${allele.change_to}`
-                formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${end}`
+                formatted.hgvsg = `g.${start}_${end}delins${allele.change_to}`
+                formatted.genomicPosition = `${allele.chromosome}:${start}-${end}`
             }
             break
         case 'ins':
-            formatted.hgvsg = `g.${start + 1}_${end + 1}ins${allele.change_to}`
-            formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${end + 1}`
+            formatted.hgvsg = `g.${start}_${end + 1}ins${allele.change_to}`
+            formatted.genomicPosition = `${allele.chromosome}:${start}-${end + 1}`
             break
         case 'dup':
-            formatted.hgvsg = `g.${start + 1}_${cnvEnd}dup`
-            formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${cnvEnd}`
+            formatted.hgvsg = `g.${start}_${cnvEnd}dup`
+            formatted.genomicPosition = `${allele.chromosome}:${start}-${cnvEnd}`
             break
         case 'dup_tandem':
-            formatted.hgvsg = `g.${start + 1}_${cnvEnd}dup_tandem`
-            formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${cnvEnd}`
+            formatted.hgvsg = `g.${start}_${cnvEnd}dup_tandem`
+            formatted.genomicPosition = `${allele.chromosome}:${start}-${cnvEnd}`
             break
         case 'del_me':
-            formatted.hgvsg = `g.${start + 1}_${length}del_me`
-            formatted.genomicPosition = `${allele.chromosome}:${start + 1}-${end}`
+            formatted.hgvsg = `g.${start}_${length}del_me`
+            formatted.genomicPosition = `${allele.chromosome}:${start}-${end}`
             break
         default:
             throw Error(`Unsupported change type detected (${allele.id}): ${allele.change_type}`)
