84c918c240a81ef6956bae0d6c195c44492c2902
scalavision <scalavision@gmail.com>
2021-10-05 16:35:22 +0200

    [alleleSidebar.js] preselect correct selectedAllele

--- src/webui/src/js/store/modules/views/workflows/alleleSidebar/actions/prepareSelectedAllele.js
+++ src/webui/src/js/store/modules/views/workflows/alleleSidebar/actions/prepareSelectedAllele.js
@@ -5,8 +5,10 @@ export default function prepareSelectedAllele({ state }) {
     const classified = state.get('views.workflows.alleleSidebar.classified')
     const technical = state.get('views.workflows.alleleSidebar.technical')
     const notRelevant = state.get('views.workflows.alleleSidebar.notRelevant')
+    const caller_type_selected = state
+        .get('views.workflows.alleleSidebar.callerTypeSelected')
+        .toUpperCase()
     let selectedAllele = state.get('views.workflows.selectedAllele')
-
     if (selectedComponent !== 'Classification') {
         state.set('views.workflows.selectedAllele', null)
         return
@@ -18,13 +20,26 @@ export default function prepareSelectedAllele({ state }) {
     }
 
     if (unclassified.length) {
-        state.set('views.workflows.selectedAllele', unclassified[0])
+        const snv_unclassified = unclassified.filter(
+            (id) => alleles[id].caller_type == caller_type_selected
+        )
+        console.log(snv_unclassified)
+        state.set('views.workflows.selectedAllele', snv_unclassified[0])
     } else if (classified.length) {
-        state.set('views.workflows.selectedAllele', classified[0])
+        const snv_classified = classified.filter(
+            (id) => alleles[id].caller_type == caller_type_selected
+        )
+        state.set('views.workflows.selectedAllele', snv_classified[0])
     } else if (technical.length) {
-        state.set('views.workflows.selectedAllele', technical[0])
+        const snv_technical = technical.filter(
+            (id) => alleles[id].caller_type == caller_type_selected
+        )
+        state.set('views.workflows.selectedAllele', snv_technical[0])
     } else if (notRelevant.length) {
-        state.set('views.workflows.selectedAllele', notRelevant[0])
+        const snv_notRelevant = notRelevant.filter(
+            (id) => alleles[id].caller_type == caller_type_selected
+        )
+        state.set('views.workflows.selectedAllele', snv_notRelevant[0])
     } else {
         state.set('views.workflows.selectedAllele', null)
     }
