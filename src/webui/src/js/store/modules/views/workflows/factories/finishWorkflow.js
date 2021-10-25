c258d94fbeaec64b8732f7274e9393c3f39240c1
scalavision <scalavision@gmail.com>
2021-10-25 16:24:39 +0200

    [cnv] improve docs on excluded_alleles_by_caller_type

--- src/webui/src/js/store/modules/views/workflows/factories/finishWorkflow.js
+++ src/webui/src/js/store/modules/views/workflows/factories/finishWorkflow.js
@@ -1,5 +1,4 @@
 import { prepareInterpretationPayload } from '../../../../common/helpers/workflow'
-import getSelectedInterpretation from '../computed/getSelectedInterpretation'
 import getAlleleIdsFromInterpretation from '../computed/getAlleleIdsFromInterpretation'
 
 const TYPES = {
@@ -27,9 +26,36 @@ export default function(finishType) {
         const excludedAlleleIdsByCallerType = state.get(
             'views.workflows.interpretation.data.filteredAlleleIds.excluded_alleles_by_caller_type'
         )
-        const excludedAlleleIds = {}
 
-        if (null != excludedAlleleIdsByCallerType) {
+        /**
+         * In the front end, we have two interpretation modes: cnv and snv, and the
+         * excluded alleles are operated on separately for each mode, input for
+         * this loop is:
+         * {
+         *   excluded_alleles_by_caller_type:
+         *   {
+         *       snv: {
+         *          region_filter: [1,2,3]
+         *          // other filters
+         *       },
+         *       cnv: {
+         *          region_filter: [23,34],
+         *          // other filters
+         *       }
+         *
+         *   }
+         * }
+         *
+         * In the backend, there are no modes, thus we need to merge common filters of the excluded
+         * alleles from the snv mode and cnv mode. Output from this loop is:
+         * {
+         *      excluded_alleles: {
+         *          region_filter: [1,2,3,23,34]
+         *      }
+         * }
+         */
+        const excludedAlleleIds = {}
+        if (excludedAlleleIdsByCallerType != null) {
             for (const excluded of Object.values(excludedAlleleIdsByCallerType)) {
                 for (const [category, allele_ids] of Object.entries(excluded)) {
                     if (!(category in excludedAlleleIds)) {
