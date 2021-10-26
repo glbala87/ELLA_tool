d839007047b1c7eb158df02c61828b442bae1b20
Øyvind Evju <oyvind.evju@medisin.uio.no>
2021-10-26 10:16:25 +0200

    Formatting

--- src/api/config/config.py
+++ src/api/config/config.py
@@ -118,12 +118,15 @@ config.setdefault("transcripts", {})["consequences"] = [
 config["acmg"] = acmgconfig
 config["custom_annotation"] = customannotationconfig
 
+
 def feature_is_enabled(feature):
     return config["app"].get("feature_flags", {}).get(feature, False)
+
 
 class FeatureNotEnabledError(RuntimeError):
     def __init__(self, feature):
         super().__init__(f"Feature '{feature}' is not enabled.")
+
 
 def get_user_config(app_config, usergroup_config, user_config):
     # Use json instead of copy.deepcopy for performance
