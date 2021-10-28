dfca98ad11576640d2bafa0c6024d1e043e73fb2
Florian Krull <flo.krull@gmail.com>
2021-10-28 16:07:59 +0200

    [api] AlleleByGeneList sorted aggregate of allele_ids by gene

--- src/api/v1/resources/allele.py
+++ src/api/v1/resources/allele.py
@@ -1,6 +1,7 @@
-from collections import defaultdict
 from flask import request
-from sqlalchemy import text
+from sqlalchemy import text, func
+from sqlalchemy.dialects.postgresql import aggregate_order_by
+
 from vardb.datamodel import sample, genotype, allele, gene, annotationshadow
 
 from api import schemas
@@ -157,31 +158,41 @@ class AlleleByGeneListResource(LogRequestResource):
         if inclusion_regex:
             filters.append(text("transcript ~ :reg").params(reg=inclusion_regex))
 
+        # deduplicate entries
         allele_id_genes = (
             session.query(
-                annotationshadow.AnnotationShadowTranscript.allele_id,
                 annotationshadow.AnnotationShadowTranscript.symbol,
                 annotationshadow.AnnotationShadowTranscript.hgnc_id,
+                allele.Allele.id.label("allele_id"),
+                allele.Allele.chromosome,
+                allele.Allele.start_position,
             )
             .join(allele.Allele)
             .filter(*filters)
-            .order_by(allele.Allele.chromosome, allele.Allele.start_position)
+            .order_by(
+                annotationshadow.AnnotationShadowTranscript.symbol,
+                annotationshadow.AnnotationShadowTranscript.hgnc_id,
+            )
+            .distinct()
+            .subquery()
+        )
+        # aggregate allele_ids by gene, sorted
+        allele_id_by_genes = (
+            session.query(
+                allele_id_genes.c.symbol,
+                allele_id_genes.c.hgnc_id,
+                func.array_agg(
+                    aggregate_order_by(
+                        allele_id_genes.c.allele_id,
+                        allele_id_genes.c.chromosome.asc(),
+                        allele_id_genes.c.start_position.asc(),
+                    )
+                ),
+            )
+            .group_by(allele_id_genes.c.symbol, allele_id_genes.c.hgnc_id)
             .all()
         )
-
-        pre_sorted_result = defaultdict(set)
-        for a in allele_id_genes:
-            pre_sorted_result[(a[1], a[2])].add(a[0])
-
-        result = list()
-        for key in sorted(
-            list(pre_sorted_result.keys()), key=lambda x: x[0] if x[0] is not None else ""
-        ):
-            allele_ids = pre_sorted_result[key]
-            result.append(
-                {"symbol": key[0], "hgnc_id": key[1], "allele_ids": sorted(list(allele_ids))}
-            )
-        return result
+        return allele_id_by_genes
 
 
 class AlleleAnalysisListResource(LogRequestResource):
