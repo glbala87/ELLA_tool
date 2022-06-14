c5a5f35fb65911c47d8f8e08786c2bffed9063ef
scimerc <checco74@gmail.com>
2022-06-14 16:40:55 +0200

    [testdata] Dynamicize `ALLELES`

--- ops/testdata/reset-testdata.py
+++ ops/testdata/reset-testdata.py
@@ -104,27 +104,26 @@ for gp_item in GP_DIR.iterdir():
         continue
     GENEPANELS.append(
         GenepanelInfo(
-            gp_item / f"{gp_item.name}.transcripts.csv",
-            gp_item / f"{gp_item.name}.phenotypes.csv",
+            gp_item / f"{gp_item.name}_genes_transcripts_regions.tsv",
+            gp_item / f"{gp_item.name}_phenotypes.tsv",
             *gp_item.name.split("_"),
         )
     )
 
+ALLELES: List[AlleleInfo] = []
 ANALYSES: List[AnalysisInfo] = []
 DEFAULT_TESTSET = "default"
+for an_item in (ANALYSES_DIR / DEFAULT_TESTSET).iterdir():
+    vcf_path = an_item / (an_item.name + ".vcf.gz")
+    if vcf_path.is_file():
+        vcf_relative_path = Path(ANALYSES_DIR.name) / DEFAULT_TESTSET / an_item.name / vcf_path.name
+        gp_name, gp_version = an_item.name.rsplit("_", maxsplit=1)
+        ALLELES.append(AlleleInfo(str(vcf_relative_path), (gp_name, gp_version)))
 for an_item in ANALYSES_DIR.iterdir():
     if not an_item.is_dir():
         continue
     ANALYSES.append(AnalysisInfo(an_item, an_item.name, an_item.name == DEFAULT_TESTSET))
 AVAILABLE_TESTSETS = [SPECIAL_TESTSET_SKIPPING_VCF] + [a.name for a in ANALYSES]
-
-# TODO: determine a way to not have this hard coded
-ALLELES = [
-    AlleleInfo(
-        "analyses/default/brca_sample_1.HBOC_v01/brca_sample_1.HBOC_v01.vcf.gz",
-        ("HBOC", "v01"),
-    ),
-]
 
 
 ###
