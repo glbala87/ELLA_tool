92746279990670e0678fac1a7ec246939c527eef
mceike <morten.eike@gmail.com>
2023-05-26 09:55:41 +0200

    [schema] Added `can_start` to schema

--- src/api/schemas/pydantic/v1/config.py
+++ src/api/schemas/pydantic/v1/config.py
@@ -42,6 +42,7 @@ class AlleleFinalizeRequirementsConfig(BaseModel):
 
 class AlleleWorkflowConfig(BaseModel):
     finalize_requirements: AlleleFinalizeRequirementsConfig
+    can_start: Optional[bool]
 
 
 class AnalysisFinalizeRequirementsConfig(BaseModel):
@@ -51,6 +52,7 @@ class AnalysisFinalizeRequirementsConfig(BaseModel):
 
 class AnalysisWorkflowConfig(BaseModel):
     finalize_requirements: AnalysisFinalizeRequirementsConfig
+    can_start: Optional[bool]
 
 
 class WorkflowsConfig(BaseModel):
