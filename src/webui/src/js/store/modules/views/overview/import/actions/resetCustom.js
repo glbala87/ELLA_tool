6e37eb66f64286ba04ded6c8f2ff1c302065da7d
Øyvind Evju <oyvind.evju@medisin.uio.no>
2023-06-09 15:13:59 +0200

    Do not reset selectedImportUserGroups. Avoids all of angular-cerebral trickery

--- src/webui/src/js/store/modules/views/overview/import/actions/resetCustom.js
+++ src/webui/src/js/store/modules/views/overview/import/actions/resetCustom.js
@@ -2,8 +2,4 @@ import { getAddedState } from '../getImportState'
 
 export default function resetCustom({ state }) {
     state.set('views.overview.import.custom.added', getAddedState())
-    state.set(
-        'views.overview.import.custom.selectedImportUserGroups',
-        state.get('app.user.group.import_groups').slice()
-    )
 }
